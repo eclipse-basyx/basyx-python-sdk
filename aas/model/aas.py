17393b1193da096e6b748d0965f03a6569350103
Sebastian Heppner <sebastian.heppner@plt.rwth-aachen.de>
2019-11-20 18:09:19 +0100

    add classes

--- aas/model/aas.py
+++ aas/model/aas.py
@@ -1,35 +1,72 @@
-class AssetAdministrationShell:
-    """
-    Administration Shell
 
-    LANG
+from typing import List
 
-    @:param security: TODO-add the security model
-    @:param derived_from: TODO-what does this actually do?
-    @:param asset: instance of Asset
-    @:param view: instance of class: view, containing a list of referable items
-    """
-    def __init__(self, security, asset, referables):
-        self.security = security  # ToDO
-        self.derived_from = AssetAdministrationShell  # Check does that really work this way?
-        self.asset = asset  # type: isinstance(Asset)
-        self.view = View(referables)
+from .security import Security, Submodel  # todo Remove Submodel here as soon as the real one exists
+from .util import Referable, HasKind
+# from .submodel import Submodel
 
 
-class Asset:
-    """
-    Asset of the Administration Shell
-
-    @:param asset_identification_model: instance of a submodel
-    """
-    def __init__(self, submodel):
-        self.asset_identification_model = submodel  # type: isinstance(Submodel)
+# todo: Add Inheritances
 
 
 class View:
     """
+    If needed stakeholder specific views can be defined on the elements of the AAS
 
+    todo: what does this exactly?
 
+    :param referable_list: List of referables
     """
-    def __init__(self, referables):
-        self.contained_elements = referables  # type: list[referable]
+    def __init__(self, referable_list: List[Referable]):
+        self.contained_elements: List[Referable] = referable_list
+
+
+class Asset:
+    """
+    An Asset describes meta data of an asset that is represented by an AAS
+
+    The asset may either represent an asset type or an asset instance.
+    todo: identifier
+
+    :param submodel:
+    :param kind: Kind (Type, Instance)
+    """
+    def __init__(self, submodel: Submodel,
+                 kind: HasKind):
+        self.asset_identification_model: Submodel = submodel
+        self.kind: HasKind = kind
+
+
+class ConceptDictionary:
+    """
+    Contains descriptions for elements that are used within the AAS
+
+    todo: see if there is more info about this
+
+    :param reference:
+    """
+    def __init__(self, reference: Referable):
+        self.reference: Referable = reference
+
+
+class AssetAdministrationShell:
+    """
+    An Asset Administration Shell
+
+    :param security: Definition of the security relevant aspects of the AAS (mandatory)
+    :param asset_administration_shell_parent: The reference to the AAS this AAS was derived from (can be empty)
+    :param asset: asset the AAS is representing (mandatory)
+    :param view_list: containing a list of referable items (can be empty)
+    """
+    def __init__(self, asset_administration_shell_parent: "AssetAdministrationShell",
+                 security: Security,
+                 asset: Asset,
+                 submodel: Submodel,
+                 concept_dictionary: ConceptDictionary,
+                 view_list: List[View]):
+        self.derived_from: AssetAdministrationShell = asset_administration_shell_parent
+        self.security: Security = security
+        self.asset: Asset = asset
+        self.submodel: Submodel = submodel
+        self.concept_dictionary: ConceptDictionary = concept_dictionary
+        self.view_list: List[View] = view_list
