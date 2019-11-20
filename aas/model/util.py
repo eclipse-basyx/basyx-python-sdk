da1af3d45f958a92a3c1d32a9db2220090aabaa9
Leon Möller <leon.moeller@plt.rwth-aachen.de>
2019-11-20 19:21:34 +0100

    model.util: move abstract properties to the respective __init__ function call __init__() of parent class in subclasses initialize properties with default values in abstract classes rename :var to :ivar (instance variable) in docstrings use :cvar for enum members static enumeration in enums use ABCMeta as metaclass instead of inheriting from ABC in abstract classes fix type hints add "<< abstract >>" to long description of abstract classes

--- aas/model/util.py
+++ aas/model/util.py
@@ -1,7 +1,6 @@
-# TODO: check for constraints. what to do with them?
-
-from abc import ABC, abstractmethod
-from enum import Enum, unique, auto
+import abc
+from enum import Enum, unique
+from typing import List, Optional
 
 
 @unique
@@ -9,15 +8,15 @@ class IdentifierType(Enum):
     """
     Enumeration of different types of Identifiers for global identification
 
-    :var IRDI: IRDI (International Registration Data Identifier) according to ISO29002-5 as an Identifierscheme for
+    :cvar IRDI: IRDI (International Registration Data Identifier) according to ISO29002-5 as an Identifierscheme for
                 properties and classifications.
-    :var URI: Uniform Resource Identifier
-    :var Custom: Custom identifiers like GUIDs (globally unique Identifiers)
+    :cvar URI: Uniform Resource Identifier
+    :cvar Custom: Custom identifiers like GUIDs (globally unique Identifiers)
     """
 
-    IRDI = auto()
-    URI = auto()
-    Custom = auto()
+    IRDI = 0
+    URI = 1
+    Custom = 2
 
 
 @unique
@@ -25,33 +24,33 @@ class Kind(Enum):
     """
     Enumeration for denoting whether an element is a type or an instance.
 
-    :var Type: hardware or software element which specifies the common attributes shared by all instances of the type
-    :var Instance: concrete, clearly identifiable component of a certain type
+    :cvar Type: hardware or software element which specifies the common attributes shared by all instances of the type
+    :cvar Instance: concrete, clearly identifiable component of a certain type
     """
 
-    Type = auto()
-    Instance = auto()
+    Type = 0
+    Instance = 1
 
 
 class AdministrativeInformation:
     """
     Administrative meta-information for an element like version information.
 
-    :var version: Version of the element.
-    :var revision: Revision of the element.
+    :ivar version: Version of the element.
+    :ivar revision: Revision of the element.
     """
 
-    def __init__(self, version: str or None, revision: str or None):
-        self.version: str or None = version
-        self.revision: str or None = revision
+    def __init__(self, version: Optional[str], revision: Optional[str]):
+        self.version: Optional[str] = version
+        self.revision: Optional[str] = revision
 
 
 class Identifier:
     """
     Used to uniquely identify an entity by using an identifier.
 
-    :var id: Identifier of the element. Its type is defined in id_type.
-    :var id_type: Type of the Identifier, e.g. URI, IRDI etc. The supported Identifier types are defined in
+    :ivar id: Identifier of the element. Its type is defined in id_type.
+    :ivar id_type: Type of the Identifier, e.g. URI, IRDI etc. The supported Identifier types are defined in
                    the enumeration "IdentifierType".
     """
 
@@ -60,147 +59,137 @@ class Identifier:
         self.id_type: IdentifierType = id_type
 
 
-class HasDataSpecification(ABC):
+class HasDataSpecification(metaclass=abc.ABCMeta):
     """
     Element that can have data specification templates. A template defines the additional attributes an element may or
     shall have.
 
-    :var has_data_specification: Global reference to the data specification template used by the element.
+    << abstract >>
+
+    :ivar has_data_specification: Global reference to the data specification template used by the element.
     """
 
-    @property
-    @abstractmethod
-    def has_data_specification(self) -> [str]:
-        pass
+    def __init__(self):
+        self.has_data_specification: List[str] = []
 
 
-class Referable(ABC):
+class Referable(metaclass=abc.ABCMeta):
     """
     An element that is referable by its id_short. This id is not globally unique. This id is unique within
     the name space of the element.
 
-    :var id_short: Identifying string of the element within its name space.
-    :var category: The category is a value that gives further meta information w.r.t. to the class of the element.
+    << abstract >>
+
+    :ivar id_short: Identifying string of the element within its name space.
+    :ivar category: The category is a value that gives further meta information w.r.t. to the class of the element.
                     It affects the expected existence of attributes and the applicability of constraints.
-    :var description: Description or comments on the element.
-    :var parent: Reference to the next referable parent element of the element.
+    :ivar description: Description or comments on the element.
+    :ivar parent: Reference to the next referable parent element of the element.
     """
 
-    @property
-    @abstractmethod
-    def id_short(self) -> str or None:
-        pass
-
-    @property
-    @abstractmethod
-    def category(self) -> str or None:
-        pass
-
-    @property
-    @abstractmethod
-    def description(self) -> str or None:
-        pass
-
-    @property
-    @abstractmethod
-    def parent(self) -> 'Referable' or None:
-        pass
+    def __init__(self):
+        self.id_short: Optional[str] = None
+        self.category: Optional[str] = None
+        self.description: Optional[str] = None
+        self.parent: Optional["Referable"] = None
 
 
-class Identifiable(Referable):
+class Identifiable(Referable, metaclass=abc.ABCMeta):
     """
     An element that has a globally unique identifier.
 
-    :var administration: Administrative information of an identifiable element.
-    :var identification: The globally unique identification of the element.
+    << abstract >>
+
+    :ivar administration: Administrative information of an identifiable element.
+    :ivar identification: The globally unique identification of the element.
     """
 
-    @property
-    @abstractmethod
-    def administration(self) -> AdministrativeInformation or None:
-        pass
-
-    @property
-    @abstractmethod
-    def identification(self) -> 'Identifier':
-        pass
+    def __init__(self):
+        super().__init__()
+        self.administration: Optional[AdministrativeInformation] = None
+        self.identification: Identifier = Identifier("", IdentifierType.IRDI)
 
 
-class HasSemantics(ABC):
+class HasSemantics(metaclass=abc.ABCMeta):
     """
     Element that can have a semantic definition.
 
-    :var semantic_id: Identifier of the semantic definition of the element. It is called semantic id of the element.
+    << abstract >>
+
+    :ivar semantic_id: Identifier of the semantic definition of the element. It is called semantic id of the element.
                        The semantic id may either reference an external global id or it may reference a referable model
                        element of kind=Type that defines the semantics of the element.
     """
 
-    @property
-    @abstractmethod
-    def semantic_id(self) -> str or None:
-        pass
+    def __init__(self):
+        self.semantic_id: Optional[str] = None
 
 
-class HasKind(ABC):
+class HasKind(metaclass=abc.ABCMeta):
     """
     An element with a kind is an element that can either represent a type or an instance.
     Default for an element is that it is representing an instance.
 
-    :var kind: Kind of the element: either type or instance. Default = Instance.
+    << abstract >>
+
+    :ivar kind: Kind of the element: either type or instance. Default = Instance.
     """
 
-    @property
-    @abstractmethod
-    def kind(self) -> Kind:
+    def __init__(self):
+        self.kind: Kind = Kind.Type
+
+
+class Constraint(metaclass=abc.ABCMeta):
+    """
+    A constraint is used to further qualify an element.
+
+    << abstract >>
+    """
+
+    def __init__(self):
         pass
 
 
-class Constraint(ABC):
-    """
-    A constraint is used to further qualify an element.
-    """
-
-    pass
-
-
-class Qualifiable(ABC):
+class Qualifiable(metaclass=abc.ABCMeta):
     """
     The value of a qualifiable element may be further qualified by one or more qualifiers or complex formulas.
 
-    :var qualifier: Additional qualification of a qualifiable element.
+    << abstract >>
+
+    :ivar qualifier: Additional qualification of a qualifiable element.
     """
 
-    @property
-    @abstractmethod
-    def qualifier(self) -> Constraint:
-        pass
+    def __init__(self):
+        self.qualifier: List[Constraint] = []
 
 
 class Formula(Constraint):
     """
     A formula is used to describe constraints by a logical expression.
 
-    :var depends_on: A formula may depend on referable or even external global elements - assumed that can be
-                        referenced and their value may be evaluated - that are used in the logical expression.
+    :ivar depends_on: A formula may depend on referable or even external global elements - assumed that can be
+                      referenced and their value may be evaluated - that are used in the logical expression.
     """
 
-    def __init__(self, depends_on: [str]):
-        self.depends_on: [str] = depends_on
+    def __init__(self, depends_on: List[str]):
+        super().__init__()
+        self.depends_on: List[str] = depends_on
 
 
 class Qualifier(Constraint, HasSemantics):
     """
     A qualifier is a type-value pair that makes additional statements w.r.t. the value of the element.
 
-    :var qualifier_type: The qualifier_type describes the type of the qualifier that is applied to the element.
-    :var qualifier_value: The qualifier_value is the value of the qualifier.
-    :var qualifier_value_id: Reference to the global unqiue id of a coded value.
-    :var semantic_id: The semantic_id defined in the HasSemantics class.
+    :ivar qualifier_type: The qualifier_type describes the type of the qualifier that is applied to the element.
+    :ivar qualifier_value: The qualifier_value is the value of the qualifier.
+    :ivar qualifier_value_id: Reference to the global unique id of a coded value.
+    :ivar semantic_id: The semantic_id defined in the HasSemantics class.
     """
 
-    def __init__(self, qualifier_type: str, qualifier_value: str or None, qualifier_value_id: str or None,
-                 semantic_id: str or None):
+    def __init__(self, qualifier_type: str, qualifier_value: Optional[str], qualifier_value_id: Optional[str],
+                 semantic_id: Optional[str]):
+        super().__init__()
         self.qualifier_type: str = qualifier_type
-        self.qualifier_value: str or None = qualifier_value
-        self.qualifier_value_id: str or None = qualifier_value_id
-        self.semantic_id: str or None = semantic_id
+        self.qualifier_value: Optional[str] = qualifier_value
+        self.qualifier_value_id: Optional[str] = qualifier_value_id
+        self.semantic_id: Optional[str] = semantic_id
