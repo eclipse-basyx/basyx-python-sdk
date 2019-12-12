2efd1e9193fec83fc0c53029503f168e3a1a357a
Sebastian Heppner <sebastian.heppner@plt.rwth-aachen.de>
2019-12-12 15:07:18 +0100

    model: add module docstring for base.py

--- aas/model/base.py
+++ aas/model/base.py
@@ -8,6 +8,10 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
+"""
+This module implements the basic structures of the AAS metamodel, including the abstract classes and enums needed for
+the higher level classes to inherit from.
+"""
 
 import abc
 import itertools
