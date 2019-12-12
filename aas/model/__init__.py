8abb7067bd94b087f4f6a694e5ffd60c9c1bf0d9
Sebastian Heppner <sebastian.heppner@plt.rwth-aachen.de>
2019-12-12 15:30:36 +0100

    model: add package docstring

--- aas/model/__init__.py
+++ aas/model/__init__.py
@@ -8,6 +8,27 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
+"""
+This package contains a python implementation of the meta-model of the AssetAdministrationShell.
+The model is divided into 5 modules, splitting it where it in sensible parts.
+
+aas.py
+    The main module, implementing high-level structures, such as AssetAdministrationShell, Asset and ConceptDictionary.
+
+base.py
+    Basic structures of the model, including all abstract classes and enumerations. This provides inheritance for the
+    higher level structures.
+
+registry.py
+    Registries for the AAS, in order to enable resolving global identifiers; and mapping identifiers to identifiable
+    objects.
+
+security.py
+    Security model of the AAS. Currently not existing.
+
+submodel.py
+    Meta-model of the submodels and events.
+"""
 
 from .aas import *
 from .security import *
