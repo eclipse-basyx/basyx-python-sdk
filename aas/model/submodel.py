f87b360a93380f406e81fd0a7e6e9d48078cc6b4
Sebastian Heppner <sebastian.heppner@plt.rwth-aachen.de>
2019-12-12 15:11:53 +0100

    model: add module docstring of submodel.py

--- aas/model/submodel.py
+++ aas/model/submodel.py
@@ -8,6 +8,9 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
+"""
+This module contains everything needed to model Submodels and define Events according to the AAS metamodel.
+"""
 
 import abc
 from typing import Optional, Set, Iterable, TYPE_CHECKING
