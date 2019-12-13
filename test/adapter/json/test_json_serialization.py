f158bc5056b688e4e87884a93cd213b5b34d8919
Torben Deppe <t.deppe@plt.rwth-aachen.de>
2019-12-13 17:52:15 +0100

    aas.examples: add example for a nearly complete asset administration shell with all its elements

--- test/adapter/json/test_json_serialization.py
+++ test/adapter/json/test_json_serialization.py
@@ -7,6 +7,8 @@ from aas import model
 from aas.adapter.json import json_serialization, json_deserialization
 from jsonschema import validate  # type: ignore
 
+from aas.examples import example_create_aas
+
 
 class JsonSerializationTest(unittest.TestCase):
 
@@ -41,6 +43,30 @@ class JsonSerializationTest(unittest.TestCase):
         # module
         # TODO move to own test
         json_object_store = json_deserialization.read_json_aas_file(io.StringIO(json_data), failsafe=False)
+
+        # load schema
+        with open(os.path.join(os.path.dirname(__file__), 'aasJSONSchemaV2.0.json'), 'r') as json_file:
+            aas_schema = json.load(json_file)
+
+        # validate serialization against schema
+        validate(instance=json_data_new, schema=aas_schema)
+
+    def test_full_example_serialization(self) -> None:
+        asset = example_create_aas.create_example_asset()
+        concept_description = example_create_aas.create_example_concept_description()
+        concept_dictionary = example_create_aas.create_example_concept_dictionary()
+        submodel = example_create_aas.create_example_submodel()
+        asset_administration_shell = example_create_aas.create_example_asset_administration_shell(
+            concept_dictionary)
+
+        # serialize object to json
+        json_data = json.dumps({
+            'assetAdministrationShells': [asset_administration_shell],
+            'submodels': [submodel],
+            'assets': [asset],
+            'conceptDescriptions': [concept_description],
+        }, cls=json_serialization.AASToJsonEncoder)
+        json_data_new = json.loads(json_data)
 
         # load schema
         with open(os.path.join(os.path.dirname(__file__), 'aasJSONSchemaV2.0.json'), 'r') as json_file:
