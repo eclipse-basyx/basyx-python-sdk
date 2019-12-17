3b569a2753d46172ab521cfcb0a355d090063694
Torben Deppe <t.deppe@plt.rwth-aachen.de>
2019-12-17 09:02:51 +0100

    test: add test_submodel and test for entity

---
+++ test/model/test_submodel.py
@@ -0,0 +1,22 @@
+# Copyright 2019 PyI40AAS Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+import unittest
+
+from aas import model
+from aas.model import Identifier, Identifiable
+
+
+class EntityTest(unittest.TestCase):
+
+    def test_set_entity(self):
+        with self.assertRaises(ValueError):
+            obj = model.Entity(id_short='Test', entity_type=model.EntityType.SELF_MANAGED_ENTITY, statement=())
