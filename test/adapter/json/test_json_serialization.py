fd6a5209a513b1e220451ca4bda9ad3c87a56804
Michael Thies <m.thies@plt.rwth-aachen.de>
2019-12-19 16:43:53 +0100

    test: Make JSON schema validating tests optional

--- test/adapter/json/test_json_serialization.py
+++ test/adapter/json/test_json_serialization.py
@@ -23,13 +23,83 @@ from aas.examples import example_create_aas, example_create_submodel_template, \
 from test.adapter.json import example_test_serialization
 
 
-class JsonSerializationTest(unittest.TestCase):
+JSON_SCHEMA_FILE = os.path.join(os.path.dirname(__file__), 'aasJSONSchemaV2.0.json')
 
+
+class JsonSerializationDeserializationTest(unittest.TestCase):
     def test_serialize_Object(self) -> None:
         test_object = model.Property("test_id_short", "string", category="PARAMETER",
                                      description={"en-us": "Germany", "de": "Deutschland"})
         json_data = json.dumps(test_object, cls=json_serialization.AASToJsonEncoder)
 
+    def test_validate_serialization(self) -> None:
+        asset_key = (model.Key(model.KeyElements.ASSET, True, "asset", model.KeyType.CUSTOM),)
+        asset_reference = model.AASReference(asset_key, model.Asset)
+        aas_identifier = model.Identifier("AAS1", model.IdentifierType.CUSTOM)
+        submodel_key = (model.Key(model.KeyElements.SUBMODEL, True, "SM1", model.KeyType.CUSTOM),)
+        submodel_identifier = submodel_key[0].get_identifier()
+        assert(submodel_identifier is not None)
+        submodel_reference = model.AASReference(submodel_key, model.Submodel)
+        submodel = model.Submodel(submodel_identifier)
+        test_aas = model.AssetAdministrationShell(asset_reference, aas_identifier, submodel_={submodel_reference})
+
+        # serialize object to json
+        json_data = json.dumps({
+                'assetAdministrationShells': [test_aas],
+                'submodels': [submodel],
+                'assets': [],
+                'conceptDescriptions': [],
+            }, cls=json_serialization.AASToJsonEncoder)
+        json_data_new = json.loads(json_data)
+
+        # try deserializing the json string into a DictObjectStore of AAS objects with help of the json_deserialization
+        # module
+        json_object_store = json_deserialization.read_json_aas_file(io.StringIO(json_data), failsafe=False)
+
+    def test_full_example_serialization(self) -> None:
+        data = example_create_aas.create_full_example()
+        file = io.StringIO()
+        json_serialization.write_aas_json_file(file=file, data=data)
+
+        # try deserializing the json string into a DictObjectStore of AAS objects with help of the json_deserialization
+        # module
+        file.seek(0)
+        json_object_store = json_deserialization.read_json_aas_file(file, failsafe=False)
+
+    def test_submodel_template_serialization(self) -> None:
+        data: model.DictObjectStore[model.Identifiable] = model.DictObjectStore()
+        data.add(example_create_submodel_template.create_example_submodel_template())
+        file = io.StringIO()
+        json_serialization.write_aas_json_file(file=file, data=data)
+
+        # try deserializing the json string into a DictObjectStore of AAS objects with help of the json_deserialization
+        # module
+        file.seek(0)
+        json_object_store = json_deserialization.read_json_aas_file(file, failsafe=False)
+
+    def test_full_empty_example_serialization(self) -> None:
+        data = example_create_aas_mandatory_attributes.create_full_example()
+        file = io.StringIO()
+        json_serialization.write_aas_json_file(file=file, data=data)
+
+        # try deserializing the json string into a DictObjectStore of AAS objects with help of the json_deserialization
+        # module
+        file.seek(0)
+        json_object_store = json_deserialization.read_json_aas_file(file, failsafe=False)
+
+    def test_missing_serialization(self) -> None:
+        data = example_test_serialization.create_full_example()
+        file = io.StringIO()
+        json_serialization.write_aas_json_file(file=file, data=data)
+
+        # try deserializing the json string into a DictObjectStore of AAS objects with help of the json_deserialization
+        # module
+        file.seek(0)
+        json_object_store = json_deserialization.read_json_aas_file(file, failsafe=False)
+
+
+@unittest.skipUnless(os.path.exists(JSON_SCHEMA_FILE), "JSON Schema not found for validation")
+class JsonSerializationSchemaTest(unittest.TestCase):
     def test_validate_serialization(self) -> None:
         asset_key = (model.Key(model.KeyElements.ASSET, True, "asset", model.KeyType.CUSTOM),)
         asset_reference = model.AASReference(asset_key, model.Asset)
@@ -53,22 +123,18 @@ class JsonSerializationTest(unittest.TestCase):
         json_data_new = json.loads(json_data)
 
         # load schema
-        with open(os.path.join(os.path.dirname(__file__), 'aasJSONSchemaV2.0.json'), 'r') as json_file:
+        with open(JSON_SCHEMA_FILE, 'r') as json_file:
             aas_schema = json.load(json_file)
 
         # validate serialization against schema
         validate(instance=json_data_new, schema=aas_schema)
-
-        # try deserializing the json string into a DictObjectStore of AAS objects with help of the json_deserialization
-        # module
-        json_object_store = json_deserialization.read_json_aas_file(io.StringIO(json_data), failsafe=False)
 
     def test_full_example_serialization(self) -> None:
         data = example_create_aas.create_full_example()
         file = io.StringIO()
         json_serialization.write_aas_json_file(file=file, data=data)
 
-        with open(os.path.join(os.path.dirname(__file__), 'aasJSONSchemaV2.0.json'), 'r') as json_file:
+        with open(JSON_SCHEMA_FILE, 'r') as json_file:
             aas_json_schema = json.load(json_file)
 
         file.seek(0)
@@ -76,11 +142,6 @@ class JsonSerializationTest(unittest.TestCase):
 
         # validate serialization against schema
         validate(instance=json_data, schema=aas_json_schema)
-
-        # try deserializing the json string into a DictObjectStore of AAS objects with help of the json_deserialization
-        # module
-        file.seek(0)
-        json_object_store = json_deserialization.read_json_aas_file(file, failsafe=False)
 
     def test_submodel_template_serialization(self) -> None:
         data: model.DictObjectStore[model.Identifiable] = model.DictObjectStore()
@@ -88,7 +149,7 @@ class JsonSerializationTest(unittest.TestCase):
         file = io.StringIO()
         json_serialization.write_aas_json_file(file=file, data=data)
 
-        with open(os.path.join(os.path.dirname(__file__), 'aasJSONSchemaV2.0.json'), 'r') as json_file:
+        with open(JSON_SCHEMA_FILE, 'r') as json_file:
             aas_json_schema = json.load(json_file)
 
         file.seek(0)
@@ -96,18 +157,13 @@ class JsonSerializationTest(unittest.TestCase):
 
         # validate serialization against schema
         validate(instance=json_data, schema=aas_json_schema)
-
-        # try deserializing the json string into a DictObjectStore of AAS objects with help of the json_deserialization
-        # module
-        file.seek(0)
-        json_object_store = json_deserialization.read_json_aas_file(file, failsafe=False)
 
     def test_full_empty_example_serialization(self) -> None:
         data = example_create_aas_mandatory_attributes.create_full_example()
         file = io.StringIO()
         json_serialization.write_aas_json_file(file=file, data=data)
 
-        with open(os.path.join(os.path.dirname(__file__), 'aasJSONSchemaV2.0.json'), 'r') as json_file:
+        with open(JSON_SCHEMA_FILE, 'r') as json_file:
             aas_json_schema = json.load(json_file)
 
         file.seek(0)
@@ -115,18 +171,13 @@ class JsonSerializationTest(unittest.TestCase):
 
         # validate serialization against schema
         validate(instance=json_data, schema=aas_json_schema)
-
-        # try deserializing the json string into a DictObjectStore of AAS objects with help of the json_deserialization
-        # module
-        file.seek(0)
-        json_object_store = json_deserialization.read_json_aas_file(file, failsafe=False)
 
     def test_missing_serialization(self) -> None:
         data = example_test_serialization.create_full_example()
         file = io.StringIO()
         json_serialization.write_aas_json_file(file=file, data=data)
 
-        with open(os.path.join(os.path.dirname(__file__), 'aasJSONSchemaV2.0.json'), 'r') as json_file:
+        with open(JSON_SCHEMA_FILE, 'r') as json_file:
             aas_json_schema = json.load(json_file)
 
         file.seek(0)
@@ -134,8 +185,3 @@ class JsonSerializationTest(unittest.TestCase):
 
         # validate serialization against schema
         validate(instance=json_data, schema=aas_json_schema)
-
-        # try deserializing the json string into a DictObjectStore of AAS objects with help of the json_deserialization
-        # module
-        file.seek(0)
-        json_object_store = json_deserialization.read_json_aas_file(file, failsafe=False)
