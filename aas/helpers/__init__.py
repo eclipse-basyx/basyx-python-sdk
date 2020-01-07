c95b1763feab6c6d803847fdd004ac26f2732162
Torben Deppe <t.deppe@plt.rwth-aachen.de>
2020-01-07 13:18:04 +0100

    aas.helpers: update doc string

--- aas/helpers/__init__.py
+++ aas/helpers/__init__.py
@@ -1,15 +1,22 @@
 """
 This package contains functions for the creation of different kind of PyAAS-objects.
 
-create_example_ass.py
+example_ass.py
     Module for the creation of example asset administration shell, related asset, example submodels and a concept
     dictionary containing an example concept description
 
-create_example_aas_mandatory_attributes.py
+example_aas_mandatory_attributes.py
     Module for the creation of an example asset administration shell, related asset, example submodels and a concept
     dictionary containing an example concept description. All objects only contain mandatory attributes.
 
-create_example_submodel_template.py
+example_aas_missing_attributes.py
+    Module for the creation of an example asset administration shell, related asset, example submodels and a concept
+    dictionary containing an example concept description. All objects contain missing object attribute combination.
+
+example_data_specification.py
+    Module for creation of an example data specification.
+
+example_submodel_template.py
     Module for the creation of an example submodel template containing all kind of submodel elements where the kind is
     always TEMPLATE.
 """
