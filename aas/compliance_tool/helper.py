6143c4630f8872fff04aa3c972d02fa0f360b3de
Torben Deppe <t.deppe@plt.rwth-aachen.de>
2020-02-19 13:43:20 +0100

    complianceTool: First version: schema check, deserialization check, example data check

---
+++ aas/compliance_tool/helper.py
@@ -0,0 +1,96 @@
+# Copyright 2019 PyI40AAS Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+from enum import Flag, auto
+from typing import List, Tuple, Optional
+
+
+class MessageCategory(Flag):
+    ERROR = auto()
+    WARNING = auto()
+    SUCCESS = auto()
+    ALL = ERROR | WARNING | SUCCESS
+
+
+class LoggingMessage():
+    """
+    A LoggingMessage is a data structure for logging messages using the compliance tool
+    """
+
+    def __init__(self,
+                 msg: str,
+                 msg_category: str,
+                 category: MessageCategory):
+        """
+        Initializer of Asset
+
+        :param msg: Message for information in which function the msg_category occurred
+        :param msg_category: Message which occurred (error message or warning or info)
+        :param category: Kind of message occurred (Error, Warning, Info)
+        """
+        super().__init__()
+        self.msg: str = msg
+        self.msg_category: str = msg_category
+        self.category: MessageCategory = category
+
+    def __str__(self, deep=False):
+        if self.msg_category and (self.category & MessageCategory.ERROR or self.category & MessageCategory.WARNING or
+                                  deep):
+            return '{}: {}\nOccurred log messages:\n{}\n'.format(self.category.name, self.msg, self.msg_category)
+        else:
+            return '{}: {}\n'.format(self.category.name, self.msg)
+
+
+class MessageLogger():
+    """
+    A MessageLogger stores LoggingMessages and can do evaluation on them
+    :ivar messages: List of LoggingMessages
+    :ivar error: Flag: bool, if an error is in any of the LoggingMessages, otherwise false
+    """
+    def __init__(self):
+        """
+        Initializer of Asset
+        """
+        super().__init__()
+        self.messages: List[LoggingMessage] = []
+        self.error: bool = False
+
+    def add_msg(self, msg: LoggingMessage):
+        self.messages.append(msg)
+        if msg.category == MessageCategory.ERROR:
+            self.error = True
+
+    def get_messages_in_list(self, flags: MessageCategory) -> List[LoggingMessage]:
+        msg_list = []
+        for x in self.messages:
+            if x.category & flags:
+                msg_list.append(x)
+        return msg_list
+
+    def get_last_message_in_list(self, flags: MessageCategory) -> Optional[LoggingMessage]:
+        for x in reversed(self.messages):
+            if x.category & flags:
+                return x
+        return None
+
+    def get_messages_in_ordered_lists(self) -> Tuple[List[LoggingMessage], List[LoggingMessage], List[LoggingMessage]]:
+        error_list = []
+        warning_list = []
+        info_list = []
+        for x in self.messages:
+            if x.msg_category == MessageCategory.ERROR:
+                error_list.append(x)
+            elif x.msg_category == MessageCategory.WARNING:
+                warning_list.append(x)
+            elif x.msg_category == MessageCategory.SUCCESS:
+                info_list.append(x)
+            else:
+                raise ValueError()
+        return error_list, warning_list, info_list
---
+++ aas/compliance_tool/json.py
@@ -0,0 +1,185 @@
+# Copyright 2019 PyI40AAS Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+"""
+Module which offers functions to use in a confirmation tool related to json files
+
+"""
+import io
+import json
+import logging
+import os
+
+import jsonschema  # type: ignore
+
+from aas import model
+from aas.adapter.json import json_deserialization
+from aas.examples.data import example_aas
+from aas.examples.data._helper import AASDataChecker
+from aas.compliance_tool.helper import MessageLogger, LoggingMessage, MessageCategory
+
+dirname = os.path.dirname
+JSON_SCHEMA_FILE = os.path.join(dirname(dirname(dirname(__file__))), 'test\\adapter\\json\\aasJSONSchemaV2.0.json')
+
+
+class LogFilter(logging.Filter):
+    def __init__(self, level):
+        self.__level = level
+
+    def filter(self, log_record):
+        return log_record.levelno <= self.__level
+
+
+def check_schema(file_path: str, logger: MessageLogger) -> None:
+    try:
+        # open given file
+        file_to_be_checked = open(file_path, 'r', encoding='utf-8-sig')
+        # read given file and check if it is conform to the json syntax
+        json_to_be_checked = json.load(file_to_be_checked)
+    except FileNotFoundError as error:
+        logger.add_msg(LoggingMessage('Error while trying to open: {}'.format(file_path), str(error),
+                                      MessageCategory.ERROR))
+        return
+    except json.decoder.JSONDecodeError as error:
+        logger.add_msg(LoggingMessage('Unable to load json file: {}'.format(file_path), str(error),
+                                      MessageCategory.ERROR))
+        return
+    finally:
+        file_to_be_checked.close()
+
+    # load json schema
+    json_file = open(JSON_SCHEMA_FILE, 'r', encoding='utf-8-sig')
+    aas_json_schema = json.load(json_file)
+    json_file.close()
+
+    # validate given file against schema
+    try:
+        jsonschema.validate(instance=json_to_be_checked, schema=aas_json_schema)
+    except jsonschema.exceptions.ValidationError as error:
+        logger.add_msg(LoggingMessage('Validation of file {} was not successfull'.format(file_path), str(error),
+                                      MessageCategory.ERROR))
+        return
+
+    logger.add_msg(LoggingMessage('Schema check of file {} was successful'.format(file_path),
+                                  '', MessageCategory.SUCCESS))
+    return
+
+
+def check_deserialization(file_path: str, failsafe: bool, logger: MessageLogger) -> model.DictObjectStore:
+    # configure logger of json_deserialization.py to avoid consol output
+    tmp_file = io.StringIO()
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG, stream=tmp_file)
+
+    # create handler to get logger info
+    logger_deserialization = logging.getLogger(json_deserialization.__name__)
+    formatter = logging.Formatter('%(levelname)s: %(message)s')
+    file_error = io.StringIO()
+    handler_error = logging.StreamHandler(file_error)
+    handler_error.setLevel(logging.ERROR)
+    handler_error.setFormatter(formatter)
+    file_warning = io.StringIO()
+    handler_warning = logging.StreamHandler(file_warning)
+    handler_warning.setLevel(logging.WARNING)
+    handler_warning.addFilter(LogFilter(logging.WARNING))
+    handler_warning.setFormatter(formatter)
+    file_info = io.StringIO()
+    handler_info = logging.StreamHandler(file_info)
+    handler_info.setLevel(logging.INFO)
+    handler_info.addFilter(LogFilter(logging.INFO))
+    handler_info.setFormatter(formatter)
+    logger_deserialization.addHandler(handler_error)
+    logger_deserialization.addHandler(handler_warning)
+    logger_deserialization.addHandler(handler_info)
+
+    try:
+        # open given file
+        file_to_be_checked = open(file_path, 'r', encoding='utf-8-sig')
+        # read given file and check if it is conform to the json schema
+        obj_store = json_deserialization.read_json_aas_file(file_to_be_checked, failsafe)
+    except FileNotFoundError as error:
+        logger.add_msg(LoggingMessage('Could not open file: {}'.format(file_path), str(error),
+                                      MessageCategory.ERROR))
+        return model.DictObjectStore()
+    except (KeyError, TypeError) as error:
+        logger.add_msg(LoggingMessage('Could not deserialize file: {}'.format(file_path), str(error),
+                                      MessageCategory.ERROR))
+        return model.DictObjectStore()
+    finally:
+        file_to_be_checked.close()
+
+    # add logger information to output
+    if failsafe:
+        if file_error.getvalue() != "":
+            logger.add_msg(LoggingMessage('Could not deserialize file: {}'.format(file_path),
+                                          str(file_error.getvalue())+str(file_warning.getvalue()),
+                                          MessageCategory.ERROR))
+            return model.DictObjectStore()
+        else:
+            logger.add_msg(LoggingMessage('Deserialization check of file {} was successful'.format(file_path),
+                                          str(file_warning.getvalue())+str(file_info.getvalue()),
+                                          MessageCategory.SUCCESS))
+    else:
+        if file_warning.getvalue() != "":
+            logger.add_msg(LoggingMessage('Could not deserialize file: {}'.format(file_path),
+                                          str(file_warning.getvalue()), MessageCategory.ERROR))
+            return model.DictObjectStore()
+        else:
+            logger.add_msg(LoggingMessage('Deserialization check of file {} was successful'.format(file_path),
+                                          '', MessageCategory.SUCCESS))
+    return obj_store
+
+
+def check_aas_example(file_path: str, failsafe: bool, logger: MessageLogger) -> None:
+    # configure logger of json_deserialization.py to avoid consol output
+    tmp_file = io.StringIO()
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG, stream=tmp_file)
+
+    logger_example = logging.getLogger(example_aas.__name__)
+    formatter = logging.Formatter('%(levelname)s: %(message)s')
+    file_warning = io.StringIO()
+    handler_warning = logging.StreamHandler(file_warning)
+    handler_warning.setLevel(logging.WARNING)
+    handler_warning.addFilter(LogFilter(logging.WARNING))
+    handler_warning.setFormatter(formatter)
+    file_info = io.StringIO()
+    handler_info = logging.StreamHandler(file_info)
+    handler_info.setLevel(logging.INFO)
+    handler_info.addFilter(LogFilter(logging.INFO))
+    handler_info.setFormatter(formatter)
+    logger_example.addHandler(handler_warning)
+    logger_example.addHandler(handler_info)
+
+    obj_store = check_deserialization(file_path, False, logger)
+
+    if logger.error:
+        logger.add_msg(LoggingMessage('Could not check against example data cause of error in deserialization', '',
+                                      MessageCategory.ERROR))
+        return
+
+    checker = AASDataChecker(raise_immediately=(not failsafe))
+    try:
+        example_aas.check_full_example(checker, obj_store, failsafe)
+    except (KeyError, AssertionError) as error:
+        logger.add_msg(LoggingMessage('Data in file {} is not equal to example data'.format(file_path),
+                                      str(error), MessageCategory.ERROR))
+        return
+
+    if len(list(checker.failed_checks)) > 0:
+        msg = ''
+        for x in checker.failed_checks:
+            msg += '{}\n'.format(x)
+        logger.add_msg(LoggingMessage('Data in file {} is not equal to example data'.format(file_path),
+                                      msg, MessageCategory.ERROR))
+    else:
+        msg = ''
+        for x in checker.successful_checks:
+            msg += '{}\n'.format(x)
+        logger.add_msg(LoggingMessage('Data in file {} is equal to example data'.format(file_path),
+                                      msg, MessageCategory.SUCCESS))
