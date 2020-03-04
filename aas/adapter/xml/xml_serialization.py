419c1c58728ac132abd6679b8d59b9a7e0ea8420
Sebastian Heppner <sebastian.heppner@plt.rwth-aachen.de>
2020-03-04 15:14:05 +0100

    adapter.xml: remove unneccesary attr_qname in write_aas_xml_file

--- aas/adapter/xml/xml_serialization.py
+++ aas/adapter/xml/xml_serialization.py
@@ -808,11 +808,7 @@ def write_aas_xml_file(file: IO,
     ElTree.register_namespace("aas_common", "http://www.admin-shell.io/aas_common/2/0")
     ElTree.register_namespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
     ElTree.register_namespace("IEC", "http://www.admin-shell.io/IEC61360/2/0")
-    attr_qname = ElTree.QName("http://www.w3.org/2001/XMLSchema-instance", "schemaLocation")
-    root = ElTree.Element(NS_AAS+"aasenv",
-                          {attr_qname: "http://www.admin-shell.io/aas/1/0 AAS.xsd "  # type: ignore
-                                       "http://www.admin-shell.io/IEC61360/1/0IEC61360.xsd "})
-    # todo: find out what this qname really does
+    root = ElTree.Element(NS_AAS+"aasenv")
     et_asset_administration_shells = ElTree.Element(NS_AAS + "assetAdministrationShells")
     for aas_obj in asset_administration_shells:
         et_asset_administration_shells.append(asset_administration_shell_to_xml(aas_obj, namespace=NS_AAS))
