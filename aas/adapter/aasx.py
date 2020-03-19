c74a3a7f0e16bb479b34bd125ff581e05317a693
Michael Thies <mail@mhthies.de>
2020-03-19 17:30:03 +0100

    adapter.aasx: Add logging

--- aas/adapter/aasx.py
+++ aas/adapter/aasx.py
@@ -12,6 +12,7 @@
 TODO
 """
 import abc
+import logging
 import os
 import re
 from typing import Dict, Tuple, IO, Union, List, Set
@@ -21,6 +22,8 @@ from .json.json_deserialization import read_json_aas_file
 from .json.json_serialization import write_aas_json_file
 import pyecma376_2
 from ..util import traversal
+
+logger = logging.getLogger(__name__)
 
 
 # TODO allow reading thumbnail image
@@ -37,6 +40,7 @@ def read_aasx(file: Union[os.PathLike, str, IO],
     """
     # Open file as OPC package file
     try:
+        logger.debug("Opening {} as AASX pacakge for reading ...".format(file))
         reader = pyecma376_2.ZipPackageReader(file)
     except Exception as e:
         raise ValueError("{} is not a valid ECMA376-2 (OPC) file".format(file)) from e
@@ -83,12 +87,16 @@ def _parse_aas_part(part_name: str, content_type: str, file_handle: IO) -> model
     extension = part_name.split("/")[-1].split(".")[-1]
     if content_type.split(";")[0] in ("text/xml", "application/xml") or content_type == "" and extension == "xml":
         # TODO XML Deserialization
+        logger.debug("Parsing AAS objects from XML stream in OPC part {} ...".format(part_name))
         raise NotImplementedError("XML deserialization is not implemented yet. Thus, AASX files with XML parts are not "
                                   "supported.")
     elif content_type.split(";")[0] in ("text/json", "application/json") or content_type == "" and extension == "json":
+        logger.debug("Parsing AAS objects from JSON stream in OPC part {} ...".format(part_name))
         return read_json_aas_file(file_handle)
     else:
-        raise ValueError("Unknown Content Type '{}' and extension '{}' of part {}"
+        logger.error("Could not determine part format of AASX part {}".format(part_name))
+        # TODO failsafe mode?
+        raise ValueError("Unknown Content Type '{}' and extension '{}' of AASX part {}"
                          .format(content_type, extension, part_name))
 
 
@@ -107,6 +115,7 @@ def _collect_supplementary_files(reader: pyecma376_2.package_model.OPCPackageRea
                 if isinstance(element, model.File):
                     absolute_name = pyecma376_2.package_model.part_realpath(element.value, part_name)
                     element.value = absolute_name
+                    logger.debug("Reading supplementary file {} from AASX package ...".format(absolute_name))
                     if absolute_name not in file_store:
                         with reader.open_part(absolute_name) as p:
                             file_store.add_file(absolute_name, p, reader.get_content_type(absolute_name))
@@ -134,16 +143,20 @@ def write_aasx(file: str,
 
     # Open OPC package file
     try:
+        logger.debug("Creating AASX package in {} ...".format(file))
         writer = pyecma376_2.ZipPackageWriter(file)
     except Exception as e:
         raise ValueError("{} is not a valid ECMA376-2 (OPC) file".format(file)) from e
 
     with writer:
         # Create AASX origin part, CoreProperties part, and root relationships
+        logger.debug("Creating AASX origin part in AASX package ...")
         p = writer.open_part(AASX_ORIGIN_PART_NAME, "text/plain")
         p.close()
+        logger.debug("Writing core properties to AASX package ...")
         with writer.open_part(pyecma376_2.DEFAULT_CORE_PROPERTIES_NAME, "application/xml") as p:
             core_properties.write_xml(p)
+        logger.debug("Writing package relationships to AASX package ...")
         writer.write_relationships((
             pyecma376_2.OPCRelationship("r1", "http://www.admin-shell.io/aasx/relationships/aasx-origin",
                                         AASX_ORIGIN_PART_NAME,
@@ -184,6 +197,7 @@ def write_aasx(file: str,
                         pass
 
             # Write AAS part
+            logger.debug("Writing AAS {} to part {} in AASX package ...".format(aas.identification, aas_part_name))
             with writer.open_part(aas_part_name, "application/json") as p:
                 write_aas_json_file(p, objects_to_be_written)
 
@@ -200,6 +214,8 @@ def write_aasx(file: str,
                 submodel_friendly_name = aas_friendlyfier.get_friendly_name(submodel.identification)
                 submodel_part_name = "/aasx/{0}/{1}/{1}.submodel.json".format(aas_friendly_name, submodel_friendly_name)
                 submodel_part_names.append(submodel_part_name)
+                logger.debug("Writing Submodel {} to part {} in AASX package ..."
+                             .format(submodel.identification, submodel_part_name))
                 with writer.open_part(submodel_part_name, "application/json") as p:
                     write_aas_json_file(p, submodel_file_objects)
 
@@ -213,15 +229,19 @@ def write_aasx(file: str,
                         try:
                             content_type = files.get_content_type(file_name)
                         except KeyError:
-                            # TODO log warning
+                            logger.warning("Could not find file {} in file store, referenced from {}."
+                                           .format(file_name, element))
                             continue
                         # TODO avoid double writes of same file
+                        logger.debug("Writing supplementary file {} to AASX package ...".format(file_name))
                         with writer.open_part(file_name, content_type) as p:
                             files.write_file(file_name, p)
                         submodel_file_names.append(pyecma376_2.package_model.normalize_part_name(file_name))
 
                 # Add relationships from submodel to supplementary parts
                 # TODO should the releationships be added from the AAS instead?
+                logger.debug("Writing aas-suppl relationships for Submodel {} to AASX package ..."
+                             .format(submodel.identification))
                 writer.write_relationships(
                     # FIXME: Count relationship ids up
                     (pyecma376_2.OPCRelationship("r1", "http://www.admin-shell.io/aasx/relationships/aas-suppl",
@@ -230,8 +250,9 @@ def write_aasx(file: str,
                      for submodel_file_name in submodel_file_names),
                     submodel_part_name)
 
-
             # Add relationships from AAS part to submodel parts
+            logger.debug("Writing aasx-spec-split relationships for AAS {} to AASX package ..."
+                         .format(aas.identification))
             writer.write_relationships(
                 # FIXME: Count relationship ids up
                 (pyecma376_2.OPCRelationship("r1", "http://www.admin-shell.io/aasx/relationships/aasx-spec-split",
@@ -241,6 +262,7 @@ def write_aasx(file: str,
                 aas_part_name)
 
         # Add relationships from AASX-origin part to AAS parts
+        logger.debug("Writing aasx-spec relationships to AASX package ...")
         writer.write_relationships(
             # FIXME: Count relationship ids up
             (pyecma376_2.OPCRelationship("r1", "http://www.admin-shell.io/aasx/relationships/aasx-spec",
