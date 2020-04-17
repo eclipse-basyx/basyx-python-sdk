8ecce6e031c9f8f6a29c5180cdcbb970ce2816f8
Leon Möller <leon.moeller@plt.rwth-aachen.de>
2020-04-17 04:23:31 +0200

    set correct copyright year for all files
    add set_copyright_year.sh script to etc/scripts

--- aas/examples/data/_helper.py
+++ aas/examples/data/_helper.py
@@ -1,4 +1,4 @@
-# Copyright 2019 PyI40AAS Contributors
+# Copyright 2020 PyI40AAS Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
 # the License. You may obtain a copy of the License at
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2019 PyI40AAS Contributors
+# Copyright 2020 PyI40AAS Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
 # the License. You may obtain a copy of the License at
