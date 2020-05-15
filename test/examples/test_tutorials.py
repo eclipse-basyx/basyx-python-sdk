de5293120f4d425d322b35b384553a450abada43
Michael Thies <m.thies@plt.rwth-aachen.de>
2020-05-15 11:56:00 +0200

    test: Add test for dynamic model tutorial

--- test/examples/test_tutorials.py
+++ test/examples/test_tutorials.py
@@ -13,7 +13,14 @@ Tests for the tutorials
 
 Functions to test if a tutorial is executable
 """
+import os
+import re
+import tempfile
 import unittest
+from contextlib import contextmanager
+
+from aas import model
+from aas.adapter.json import read_aas_json_file
 
 
 class TutorialTest(unittest.TestCase):
@@ -25,3 +32,47 @@ class TutorialTest(unittest.TestCase):
 
     def test_tutorial_serialization_deserialization_json(self):
         from aas.examples import tutorial_serialization_deserialization_json
+
+    def test_tutorial_dynamic_model(self) -> None:
+        with temporary_workingdirectory():
+            from aas.examples import tutorial_dynamic_model
+
+            # After executing the tutorial, there should be an AAS JSON file in the temporary working directory,
+            # containing a list of processes, which should at least contain one "python" process.
+            with open('ComputerInformationTest.json') as f:
+                objects = read_aas_json_file(f, failsafe=False)
+
+        submodel = objects.get_identifiable(model.Identifier('https://acplt.org/ComputerInformationTest',
+                                                             model.IdentifierType.IRI))
+        assert(isinstance(submodel, model.Submodel))
+        process_list = submodel.get_referable('processes')
+        assert(isinstance(process_list, model.SubmodelElementCollection))
+        processes = [(p.get_referable('pid').value,   # type: ignore
+                      p.get_referable('name').value,   # type: ignore
+                      p.get_referable('mem').value)   # type: ignore
+                     for p in process_list.value]
+        r = re.compile(r'[Pp]ython|coverage')  # When tests are run via `coverage`, there might not be a Python process
+        python_processes = list(filter(lambda p: r.match(p[1]), processes))
+        self.assertTrue(len(python_processes) > 0,
+                        "'Python' not found in Process list {}".format([p[1] for p in processes]))
+        self.assertGreater(python_processes[0][0], 0)
+        self.assertGreater(python_processes[0][2], 0.0)
+
+
+@contextmanager
+def temporary_workingdirectory():
+    """
+    A helper contextmanager to temporarily change the current working directory of the Python process to a temporary
+    directory.
+
+    The temp directory is deleted with all its contents when leaving the with-context. This can be used to test Python
+    scripts, which write files to the current working directory.
+    """
+    cwd = os.getcwd()
+    tempdir = tempfile.TemporaryDirectory()
+    os.chdir(tempdir.name)
+    try:
+        yield None
+    finally:
+        os.chdir(cwd)
+        tempdir.cleanup()
