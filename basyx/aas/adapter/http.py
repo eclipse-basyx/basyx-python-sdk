f56149f9048c426cd038d82ed3b9e0eed16d2db0
Leon Möller <leon.moeller@plt.rwth-aachen.de>
2020-06-14 14:12:17 +0200

    adapter.http: serialize response data in xml/json response classes
    adapter.http: add custom identifier converter for werkzeug
    adapter.http: restructure routing map, add first submodel route
    adapter.http: refine imports

--- basyx/aas/adapter/http.py
+++ basyx/aas/adapter/http.py
@@ -9,90 +9,122 @@
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-import base64
-from binascii import Error as Base64Error
-from werkzeug.datastructures import MIMEAccept, Headers
-from werkzeug.exceptions import BadRequest, HTTPException, NotAcceptable, NotImplemented
-from werkzeug.http import parse_accept_header
-from werkzeug.routing import Map, Rule, Submount
+import json
+from lxml import etree  # type: ignore
+import werkzeug
+from werkzeug.exceptions import BadRequest, NotAcceptable, NotFound, NotImplemented
+from werkzeug.routing import Rule, Submount
 from werkzeug.wrappers import Request, Response
 
 from .. import model
 from ._generic import IDENTIFIER_TYPES_INVERSE
-from .json.json_serialization import asset_administration_shell_to_json
+from .json import json_serialization
+from .xml import xml_serialization
 
 from typing import Dict, Iterable, Optional, Type
 
 
-class JsonResponse(Response):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs, content_type="application/json")
+def xml_element_to_str(element: etree.Element) -> str:
+    # namespaces will just get assigned a prefix like nsX, where X is a positive integer
+    # "aas" would be a better prefix for the AAS namespace
+    # TODO: find a way to specify a namespace map when serializing
+    return etree.tostring(element, xml_declaration=True, encoding="utf-8")
 
 
-class XmlResponse(Response):
+class APIResponse(Response):
+    def __init__(self, data, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if isinstance(data, model.AssetAdministrationShell):
+            self.data = self.serialize_aas(data)
+        elif isinstance(data, model.Submodel):
+            self.data = self.serialize_sm(data)
+        # TODO: encode non-data responses with json/xml as well (e.g. results and errors)
+
+    def serialize_aas(self, aas: model.AssetAdministrationShell) -> str:
+        pass
+
+    def serialize_sm(self, aas: model.Submodel) -> str:
+        pass
+
+
+class JsonResponse(APIResponse):
+    def __init__(self, *args, content_type="application/json", **kwargs):
+        super().__init__(*args, **kwargs, content_type=content_type)
+
+    def serialize_aas(self, aas: model.AssetAdministrationShell) -> str:
+        return json.dumps(aas, cls=json_serialization.AASToJsonEncoder)
+
+    def serialize_sm(self, sm: model.Submodel) -> str:
+        return json.dumps(sm, cls=json_serialization.AASToJsonEncoder)
+
+
+class XmlResponse(APIResponse):
     def __init__(self, *args, content_type="application/xml", **kwargs):
         super().__init__(*args, **kwargs, content_type=content_type)
 
+    def serialize_aas(self, aas: model.AssetAdministrationShell) -> str:
+        return xml_element_to_str(xml_serialization.asset_administration_shell_to_xml(aas))
+
+    def serialize_sm(self, sm: model.Submodel) -> str:
+        return xml_element_to_str(xml_serialization.submodel_to_xml(sm))
+
 
 class XmlResponseAlt(XmlResponse):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs, content_type="text/xml")
+    def __init__(self, *args, content_type="text/xml", **kwargs):
+        super().__init__(*args, **kwargs, content_type=content_type)
 
 
 """
 A mapping of supported content types to their respective ResponseType.
-The first content type in this dict will be preferred if the requester doesn't specify preferred content types using
-and HTTP-"Accept" header.
+The first content type in this dict will be preferred if the requester
+doesn't specify preferred content types using the HTTP Accept header.
 """
 RESPONSE_TYPES = {
     "application/json": JsonResponse,
-    # "application/xml": XmlResponse,
-    # "text/xml": XmlResponseAlt
+    "application/xml": XmlResponse,
+    "text/xml": XmlResponseAlt
 }
+
+
+class IdentifierConverter(werkzeug.routing.PathConverter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def to_python(self, value) -> model.Identifier:
+        id_type, id_ = super().to_python(value).split(":", 1)
+        try:
+            return model.Identifier(id_, IDENTIFIER_TYPES_INVERSE[id_type])
+        except KeyError:
+            raise BadRequest(f"'{id_type}' is not a valid identifier type!")
 
 
 class WSGIApp:
     def __init__(self, object_store: model.AbstractObjectStore):
         self.object_store: model.AbstractObjectStore = object_store
-        self.url_map: Map = Map([
+        self.url_map = werkzeug.routing.Map([
             Submount("/api/v1", [
-                # TODO: custom decoder for base64
-                Rule("/shells/<string:aas_id>/aas", methods=["GET"], endpoint=self.get_aas),
-                Rule("/shells/<string:aas_id>/abc")  # no endpoint => 501 not implemented
+                Submount("/shells/<identifier:aas_id>", [
+                    Rule("/aas", methods=["GET"], endpoint=self.get_aas)
+                ]),
+                Submount("/submodels/<identifier:sm_id>", [
+                    Rule("/submodel", methods=["GET"], endpoint=self.get_sm)
+                ])
             ])
-        ])
+        ], converters={"identifier": IdentifierConverter})
 
     def __call__(self, environ, start_response):
         response = self.handle_request(Request(environ))
         return response(environ, start_response)
 
     @classmethod
-    def preferred_content_type(cls, headers: Headers, content_types: Iterable[str]) -> Optional[str]:
+    def preferred_content_type(cls, headers: werkzeug.datastructures.Headers, content_types: Iterable[str]) \
+            -> Optional[str]:
         accept_str: Optional[str] = headers.get("accept")
         if accept_str is None:
             # return first content type in case accept http header is not specified
             return next(iter(content_types))
-        accept: MIMEAccept = parse_accept_header(accept_str, MIMEAccept)
+        accept = werkzeug.http.parse_accept_header(accept_str, werkzeug.datastructures.MIMEAccept)
         return accept.best_match(content_types)
-
-    @classmethod
-    def base64_param(cls, args: Dict[str, str], param: str) -> str:
-        try:
-            b64decoded = base64.b64decode(args[param])
-        except Base64Error:
-            raise BadRequest(f"URL-Parameter '{param}' with value '{args[param]}' is not a valid base64 string!")
-        try:
-            return b64decoded.decode("utf-8")
-        except UnicodeDecodeError:
-            raise BadRequest(f"URL-Parameter '{param}' with base64 decoded value '{b64decoded!r}' is not valid utf-8!")
-
-    @classmethod
-    def identifier_from_param(cls, args: Dict[str, str], param: str) -> model.Identifier:
-        id_type, id_ = cls.base64_param(args, param).split(":", 1)
-        try:
-            return model.Identifier(id_, IDENTIFIER_TYPES_INVERSE[id_type])
-        except KeyError:
-            raise BadRequest(f"'{id_type}' is not a valid identifier type!")
 
     # this is not used yet
     @classmethod
@@ -105,14 +137,12 @@ class WSGIApp:
     def get_obj_ts(self, identifier: model.Identifier, type_: Type[model.provider._IT]) -> model.provider._IT:
         identifiable = self.object_store.get(identifier)
         if not isinstance(identifiable, type_):
-            raise BadRequest(f"Object specified by id {identifier} is of unexpected type {type(identifiable)}! "
-                             f"Expected type: {type_}")
+            raise NotFound(f"No '{type_.__name__}' with id '{identifier}' found!")
         return identifiable
 
     def handle_request(self, request: Request):
         adapter = self.url_map.bind_to_environ(request.environ)
         # determine response content type
-        # TODO: implement xml responses
         content_type = self.preferred_content_type(request.headers, RESPONSE_TYPES.keys())
         if content_type is None:
             return NotAcceptable(f"This server supports the following content types: "
@@ -122,18 +152,23 @@ class WSGIApp:
             endpoint, values = adapter.match()
             if endpoint is None:
                 return NotImplemented("This route is not yet implemented.")
-            endpoint(request, values, response_type)
-        except HTTPException as e:
-            # raised error leaving this function => 500
+            return endpoint(request, values, response_type)
+        # any raised error that leaves this function will cause a 500 internal server error
+        # so catch raised http exceptions and return them
+        # TODO: apply response types to http exceptions
+        except werkzeug.exceptions.HTTPException as e:
             return e
 
     # http api issues (depth parameter, repository interface (id encoding))
-    def get_aas(self, request: Request, args: Dict[str, str], response_type: Type[Response]):
+    def get_aas(self, request: Request, args: Dict, response_type: Type[APIResponse]) -> APIResponse:
         # TODO: depth parameter
-        aas_id = self.identifier_from_param(args, "aas_id")
-        aas = self.get_obj_ts(aas_id, model.AssetAdministrationShell)
-        # TODO: encode with xml for xml responses
-        return response_type(asset_administration_shell_to_json(aas))
+        aas = self.get_obj_ts(args["aas_id"], model.AssetAdministrationShell)
+        return response_type(aas)
+
+    def get_sm(self, request: Request, args: Dict, response_type: Type[APIResponse]) -> APIResponse:
+        # TODO: depth parameter
+        sm = self.get_obj_ts(args["sm_id"], model.Submodel)
+        return response_type(sm)
 
 
 if __name__ == "__main__":
