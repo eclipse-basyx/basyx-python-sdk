bfae85953d95138baa89f9f783b09e231543c667
Sebastian Heppner <sebastian.heppner@plt.rwth-aachen.de>
2020-07-31 12:04:59 +0200

    tests.test_backend: Add test for _backend_store methods

--- test/test_backends.py
+++ test/test_backends.py
@@ -1,7 +1,8 @@
 from unittest import mock
 import unittest
 
-from aas import model, backends
+from aas import backends
+from aas.model import *
 
 
 class BackendTest:
@@ -9,20 +10,9 @@ class BackendTest:
 
 
 class BackendsTest(unittest.TestCase):
-    def test_mock_get_backend(self):
-        with mock.patch("model.Referable.Backend") as mock_backend:
-            with mock.patch("model.Referable.get_backend") as mock_get_backend:
-                mock_get_backend.method.return_value = mock_backend
-                test_referable = model.Referable()
-                test_referable.source = "test_source"
-                self.assertEqual(mock_backend, mock_get_backend(test_referable.source))
-                # I know this tests how mock works and not how get_backend works, but I wanted to try this. This is
-                # temporary.
-
     def test_backend_store(self):
-        with mock.patch("model.Referable.Backend") as mock_backend:
-            backends.register_backend("urn:x-test:test_backend", mock_backend)
-            self.assertEqual(backends._backends_map["urn:x-test:test_backend"], mock_backend)
+        with mock.patch("aas.backends.Backend") as mock_backend:
+            backends.register_backend("urn", mock_backend)
             self.assertEqual(backends.get_backend("urn:x-test:test_backend"), mock_backend)
 
             backends._backends_map = {}
