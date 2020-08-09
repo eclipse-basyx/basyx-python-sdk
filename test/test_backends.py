d7565921a6802423004f202e697c904410e9ed86
Sebastian Heppner <sebastian.heppner@plt.rwth-aachen.de>
2020-08-09 16:41:16 +0200

    [WIP] test_backends: Add tests for Backend.commit_object and Backend.update_object()

--- test/test_backends.py
+++ test/test_backends.py
@@ -1,12 +1,27 @@
 from unittest import mock
 import unittest
 
-from aas import backends
 from aas.model import *
+from .model import test_base
 
 
 class BackendTest:
-    pass
+    def test_backend_commit_object(self):
+        with mock.patch("aas.backends.Backend") as mock_backend:
+            backends.register_backend("mockScheme", mock_backend)
+            test_referable = test_base.ExampleReferable()
+            test_referable.source = "mockScheme:x-test:test_referable"
+            test_referable.commit()
+            mock_backend.commit_object.assert_called()
+            mock_backend.commit_object.assert_called_with(test_referable, test_referable, [])
+
+    def test_backend_update_object(self):
+        with mock.patch("aas.backends.Backend") as mock_backend:
+            backends.register_backend("mockScheme", mock_backend)
+            test_referable = test_base.ExampleReferable()
+            test_referable.source = "mockScheme:x-test:test_referable"
+            test_referable.update()
+            mock_backend.update_object.assert_called_with(test_referable, test_referable, [])
 
 
 class BackendsTest(unittest.TestCase):
