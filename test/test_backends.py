5ec77910a0227d733fdf9a05affa2445f9b8809a
Sebastian Heppner <sebastian.heppner@plt.rwth-aachen.de>
2020-08-20 12:23:24 +0200

    test.test_backends: Remove tests that are to be in test.model.test_base

--- test/test_backends.py
+++ test/test_backends.py
@@ -1,27 +1,7 @@
 from unittest import mock
 import unittest
 
-from aas.model import *
-from .model import test_base
-
-
-class BackendTest:
-    def test_backend_commit_object(self):
-        with mock.patch("aas.backends.Backend") as mock_backend:
-            backends.register_backend("mockScheme", mock_backend)
-            test_referable = test_base.ExampleReferable()
-            test_referable.source = "mockScheme:x-test:test_referable"
-            test_referable.commit()
-            mock_backend.commit_object.assert_called()
-            mock_backend.commit_object.assert_called_with(test_referable, test_referable, [])
-
-    def test_backend_update_object(self):
-        with mock.patch("aas.backends.Backend") as mock_backend:
-            backends.register_backend("mockScheme", mock_backend)
-            test_referable = test_base.ExampleReferable()
-            test_referable.source = "mockScheme:x-test:test_referable"
-            test_referable.update()
-            mock_backend.update_object.assert_called_with(test_referable, test_referable, [])
+from aas import backends
 
 
 class BackendsTest(unittest.TestCase):
