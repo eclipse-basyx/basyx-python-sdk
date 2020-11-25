fa1e3768d9bcd2e506501e7947c709771315576a
Sebastian Heppner <sebastian.heppner@plt.rwth-aachen.de>
2020-11-25 16:23:01 +0100

    model.submodel.SubmodelElement: Fix invalid escape sequence in docstring

--- aas/model/submodel.py
+++ aas/model/submodel.py
@@ -25,7 +25,7 @@ class SubmodelElement(base.Referable, base.Qualifiable, base.HasSemantics, base.
     A submodel element is an element suitable for the description and differentiation of assets.
 
     *Note:* The concept of type and instance applies to submodel elements. :class:`Properties <.Property>` are special
-    submodel elements. The property types are defined in dictionaries (like the IEC Common Data Dictionary or eCl\@ss),
+    submodel elements. The property types are defined in dictionaries (like the IEC Common Data Dictionary or eCl\\@ss),
     they do not have a value. The property type (`kind=Type`) is also called data element type in some standards.
     The property instances (`kind=Instance`) typically have a value. A property instance is also called
     property-value pair in certain standards.
