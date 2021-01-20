54be14896e2e65794e1f0311883c0f0cc2f737a0
Sebastian Heppner <sebastian.heppner@plt.rwth-aachen.de>
2021-01-20 15:41:20 +0100

    aas.util.traversal: Update docstrings for sphinx

--- aas/util/traversal.py
+++ aas/util/traversal.py
@@ -9,7 +9,7 @@
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 """
-A module with helper functions for traversing AAS object strcutures.
+A module with helper functions for traversing AAS object structures.
 """
 
 from typing import Union, Iterator
@@ -20,9 +20,12 @@ from .. import model
 def walk_submodel(collection: Union[model.Submodel, model.SubmodelElementCollection]) \
         -> Iterator[model.SubmodelElement]:
     """
-    Traverse the SubmodelElements in a Submodel or a SubmodelElementCollection recursively in post-order tree-traversal.
+    Traverse the :class:`SubmodelElements <aas.model.submodel.SubmodelElement>` in a
+    :class:`~aas.model.submodel.Submodel` or a :class:`~aas.model.submodel.SubmodelElementCollection` recursively in
+    post-order tree-traversal.
 
-    This is a generator function, yielding all the SubmodelElements. No SubmodelElements should be added, removed or
+    This is a generator function, yielding all the :class:`SubmodelElements <aas.model.submodel.SubmodelElement>`.
+    No :class:`SubmodelElements <aas.model.submodel.SubmodelElement>` should be added, removed or
     moved while iterating, as this could result in undefined behaviour.
     """
     elements = collection.submodel_element if isinstance(collection, model.Submodel) else collection.value
