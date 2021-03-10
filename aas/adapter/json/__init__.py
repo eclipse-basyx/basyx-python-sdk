6a13a08440eb68c5e31a5a1941184f82f54fb965
Sebastian Heppner <sebastian.heppner@plt.rwth-aachen.de>
2021-03-10 16:51:17 +0100

    aas.adapter.json.__init__: Update module docstring for sphinx

--- aas/adapter/json/__init__.py
+++ aas/adapter/json/__init__.py
@@ -1,16 +1,18 @@
 """
+.. _adapter.json.__init__:
+
 This package contains functionality for serialization and deserialization of PyI40AAS objects into/from JSON.
 
-json_serialization:
-    The module offers a function to write an ObjectStore to a given file and therefore defines the custom JSONEncoder
-    `AASToJsonEncoder` which handles encoding of all PyI40AAS objects and their attributes by converting them into
-    standard python objects.
+:ref:`json_serialization <adapter.json.json_serialization>`: The module offers a function to write an ObjectStore to a
+given file and therefore defines the custom JSONEncoder :class:`~.aas.adapter.json.json_serialization.AASToJsonEncoder`
+which handles encoding of all PyI40AAS objects and their attributes by converting them into standard python objects.
 
-json_deserialization.py
-    The module implements custom JSONDecoder classes `AASFromJsonDecoder` and `StrictAASFromJsonDecoder`, that — when
-    used with Python's `json` module — detect AAS objects in the parsed JSON and convert them into the corresponding
-    PyI40AAS object. A function `read_json_aas_file()` is provided to read all AAS objects within a JSON file and return
-    them as PyI40AAS ObjectStore.
+:ref:`json_deserialization <adapter.json.json_deserialization>`: The module implements custom JSONDecoder classes
+:class:`~aas.adapter.json.json_deserialization.AASFromJsonDecoder` and
+:class:`~aas.adapter.json.json_deserialization.StrictAASFromJsonDecoder`, that — when used with Python's `json`
+module — detect AAS objects in the parsed JSON and convert them into the corresponding PyI40AAS object.
+A function :meth:`~aas.adapter.json.json_deserialization.read_aas_json_file` is provided to read all AAS objects
+within a JSON file and return them as PyI40AAS ObjectStore.
 """
 import os.path
 
