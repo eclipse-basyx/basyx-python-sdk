387be8c39d90933ad0e6bbe0efd735a1dcab3765
Sebastian Heppner <sebastian.heppner@plt.rwth-aachen.de>
2021-03-30 13:05:14 +0200

    aas.util.__init__: Update module docstring for sphinx

--- aas/util/__init__.py
+++ aas/util/__init__.py
@@ -1,6 +1,9 @@
 """
 This package provides helpful utilities for working with a python based AAS model.
 
-identification.py
-    Generate identifiers
+`identification.py`:
+    Generate :class:`Identifiers <aas.model.base.Identifier>`
+
+`traversal.py`:
+    A module with helper functions for traversing AAS object structures.
 """
