0d0a0137f58697cadba40dd13dfe7262bb5ed44a
Leon Möller <leon.moeller@plt.rwth-aachen.de>
2021-10-28 05:47:07 +0200

    remove Asset
    and all references to it

--- compliance-tool/test/compliance_tool/test_compliance_check_json.py
+++ compliance-tool/test/compliance_tool/test_compliance_check_json.py
@@ -31,7 +31,7 @@ class ComplianceToolJsonTest(unittest.TestCase):
         self.assertEqual(Status.SUCCESS, manager.steps[0].status)
         self.assertEqual(Status.FAILED, manager.steps[1].status)
         self.assertEqual(Status.NOT_EXECUTED, manager.steps[2].status)
-        self.assertIn("Expecting ',' delimiter: line 5 column 2 (char 69)", manager.format_step(1, verbose_level=1))
+        self.assertIn("Expecting ',' delimiter: line 4 column 2 (char 54)", manager.format_step(1, verbose_level=1))
 
         manager.steps = []
         file_path_3 = os.path.join(script_dir, 'files/test_missing_submodels.json')
