2c806c123ea17cd6de40845f81da832c03fe581c
Michael Thies <m.thies@plt.rwth-aachen.de>
2022-02-02 12:14:16 +0100

    docs: Fix minor formatting syntax issues

--- basyx/aas/adapter/__init__.py
+++ basyx/aas/adapter/__init__.py
@@ -2,8 +2,8 @@
 This package contains different kinds of adapters.
 
 * :ref:`json <adapter.json.__init__>`: This package offers an adapter for serialization and deserialization of BaSyx
-Python SDK objects to/from JSON.
+  Python SDK objects to/from JSON.
 * :ref:`xml <adapter.xml.__init__>`: This package offers an adapter for serialization and deserialization of BaSyx
-Python SDK objects to/from XML.
+  Python SDK objects to/from XML.
 * :ref:`aasx <adapter.aasx>`: This package offers functions for reading and writing AASX-files.
 """
