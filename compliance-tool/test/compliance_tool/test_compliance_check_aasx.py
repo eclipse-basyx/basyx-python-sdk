1dcd364f002528d126ea165564f3541b77b4417f
Sebastian Heppner <s.heppner@plt.rwth-aachen.de>
2022-04-06 09:41:20 +0200

    test.compliance_tool.test_compliance_check_aasx: Skip schema test for Python <= 3.6

--- compliance-tool/test/compliance_tool/test_compliance_check_aasx.py
+++ compliance-tool/test/compliance_tool/test_compliance_check_aasx.py
@@ -132,7 +132,7 @@ class ComplianceToolAASXTest(unittest.TestCase):
                       manager.format_step(4, verbose_level=1))
         self.assertEqual(Status.NOT_EXECUTED, manager.steps[5].status)
 
-    @unittest.skipIf((3, 6) <= sys.version_info < (3, 7), "The XML schema check fails for Python 3.6")
+    @unittest.skipIf(sys.version_info < (3, 7), "The XML schema check fails for Python <= 3.6")
     def test_check_schema(self):
         manager = ComplianceToolStateManager()
         script_dir = os.path.dirname(__file__)
