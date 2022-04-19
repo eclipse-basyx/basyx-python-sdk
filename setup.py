b32767d9cc3666c0b1fc22bd23157a9da7ee92fb
Leon Möller <leon.moeller@plt.rwth-aachen.de>
2022-04-19 19:01:26 +0200

    Update license headers for MIT license

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # Copyright (c) 2019-2021 the Eclipse BaSyx Authors
 #
-# This program and the accompanying materials are made available under the terms of the Eclipse Public License v. 2.0
-# which is available at https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0 which is available
-# at https://www.apache.org/licenses/LICENSE-2.0.
+# This program and the accompanying materials are made available under the terms of the MIT License, available in
+# the LICENSE file of this project.
 #
-# SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+# SPDX-License-Identifier: MIT
 
 import setuptools
 
