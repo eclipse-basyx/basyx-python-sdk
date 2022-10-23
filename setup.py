632124d3c1b7121ae07ea7e11fac722dbab78b64
Leon Möller <leon.moeller@plt.rwth-aachen.de>
2022-10-23 02:02:03 +0200

    chore: drop support for Python 3.6
    In order to update the dependency on jsonschema
    (80913ef9ad5dc1bbf673b84acbc54dbfc1a3915f), Python 3.6 must be dropped.
    Python 3.6 is EOL since 23.12.2021 and all major distributions have
    updated to more recent releases.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setuptools.setup(
             "aas-compliance-check = basyx.aas.compliance_tool.cli:main"
         ]
     },
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
         'python-dateutil>=2.8,<3',
         'lxml>=4.2,<5',
