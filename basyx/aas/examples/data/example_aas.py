6e53d55511520a8bcb5d553b5b146e7c33f062e1
s-heppner <s.heppner@plt.rwth-aachen.de>
2023-06-29 17:47:21 +0200

    Clean up codestyle (to be squashed)

--- basyx/aas/examples/data/example_aas.py
+++ basyx/aas/examples/data/example_aas.py
@@ -648,7 +648,6 @@ def create_example_submodel() -> model.Submodel:
         semantic_id=model.GlobalReference((model.Key(type_=model.KeyTypes.GLOBAL_REFERENCE,
                                                      value='http://acplt.org/Events/ExampleBasicEventElement'),)),
         qualifier=(),
-
         extension=(),
         supplemental_semantic_id=(),
         embedded_data_specifications=()
