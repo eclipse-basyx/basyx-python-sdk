b1121213e20cc9c626cb0fd1752f72be214ddd0c
Leon Möller <leon.moeller@plt.rwth-aachen.de>
2023-07-19 19:28:30 +0200

    test: fix accidentally disabled tests
    If an assertion is contained within the same context as a statement,
    that raises an error, the assertion is never executed, because the
    raised error leaves the context.

--- test/backend/test_couchdb.py
+++ test/backend/test_couchdb.py
@@ -38,10 +38,10 @@ class CouchDBBackendOfflineMethodsTest(unittest.TestCase):
 
         with self.assertRaises(couchdb.CouchDBSourceError) as cm:
             couchdb.CouchDBBackend._parse_source("wrong_scheme:plt.rwth-aachen.couchdb:5984/path_to_db/path_to_doc")
-            self.assertEqual("Source has wrong format. "
-                             "Expected to start with {couchdb, couchdbs}, got "
-                             "{wrong_scheme:plt.rwth-aachen.couchdb:5984/path_to_db/path_to_doc}",
-                             cm.exception)
+        self.assertEqual("Source has wrong format. "
+                         "Expected to start with {couchdb://, couchdbs://}, got "
+                         "{wrong_scheme:plt.rwth-aachen.couchdb:5984/path_to_db/path_to_doc}",
+                         str(cm.exception))
 
 
 @unittest.skipUnless(COUCHDB_OKAY, "No CouchDB is reachable at {}/{}: {}".format(TEST_CONFIG['couchdb']['url'],
