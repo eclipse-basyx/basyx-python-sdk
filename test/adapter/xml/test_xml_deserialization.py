662ccf6ad05254e0b2965f2ff643efcd1005d2fa
Leon Möller <leon.moeller@plt.rwth-aachen.de>
2023-11-03 16:55:14 +0100

    test.adapter.xml.xml_deserialization: make `OperationVariable` tests more explicit

--- test/adapter/xml/test_xml_deserialization.py
+++ test/adapter/xml/test_xml_deserialization.py
@@ -232,7 +232,7 @@ class XmlDeserializationTest(unittest.TestCase):
             </aas:submodel>
         </aas:submodels>
         """)
-        self._assertInExceptionAndLog(xml, "aas:value", KeyError, logging.ERROR)
+        self._assertInExceptionAndLog(xml, ["aas:value", "has no submodel element"], KeyError, logging.ERROR)
 
     def test_operation_variable_too_many_submodel_elements(self) -> None:
         # TODO: simplify this should our suggestion regarding the XML schema get accepted
@@ -266,6 +266,7 @@ class XmlDeserializationTest(unittest.TestCase):
         with self.assertLogs(logging.getLogger(), level=logging.WARNING) as context:
             read_aas_xml_file(io.BytesIO(xml.encode("utf-8")), failsafe=False)
         self.assertIn("aas:value", context.output[0])
+        self.assertIn("more than one submodel element", context.output[0])
 
     def test_duplicate_identifier(self) -> None:
         xml = _xml_wrap("""
