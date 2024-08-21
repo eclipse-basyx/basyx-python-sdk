b32c97cd4b0187958c7a55b788b21a2f601524c2
zrgt <garmaev@gmx.de>
2024-08-21 23:34:19 +0200

    Revert "app: support JSON and XML files "
    This reverts commit 31f9ea35dbbdeb2d73e942faed4ec28c8f30d910.

--- server/app/main.py
+++ server/app/main.py
@@ -2,13 +2,13 @@ import os
 import pathlib
 import sys
 
-from basyx.aas import model, adapter
+from basyx.aas import model
 from basyx.aas.adapter import aasx
 
 from basyx.aas.backend.local_file import LocalFileObjectStore
 from basyx.aas.adapter.http import WSGIApp
 
-storage_path = os.getenv("STORAGE_PATH", "storage")
+storage_path = os.getenv("STORAGE_PATH", "/storage")
 storage_type = os.getenv("STORAGE_TYPE", "AASX")
 base_path = os.getenv("API_BASE_PATH")
 
@@ -20,27 +20,18 @@ if base_path is not None:
 if storage_type == "LOCAL_FILE":
     application = WSGIApp(LocalFileObjectStore(storage_path), aasx.DictSupplementaryFileContainer(), **wsgi_optparams)
 
-elif storage_type in ("AASX", "JSON", "XML"):
+elif storage_type == "AASX":
     object_store: model.DictObjectStore = model.DictObjectStore()
     file_store: aasx.DictSupplementaryFileContainer = aasx.DictSupplementaryFileContainer()
 
-    suffix_pattern = f"*.{storage_type.lower()}"
-    for file in pathlib.Path(storage_path).glob(suffix_pattern):
+    for file in pathlib.Path(storage_path).glob("*.aasx"):
         if not file.is_file():
             continue
         print(f"Loading {file}")
-
-        if storage_type == "JSON":
-            with open(file) as f:
-                adapter.json.read_aas_json_file_into(object_store, f)
-        elif storage_type == "XML":
-            with open(file) as f:
-                adapter.xml.read_aas_xml_file_into(object_store, file)
-        else:
-            with aasx.AASXReader(file) as reader:
-                reader.read_into(object_store=object_store, file_store=file_store)
+        with aasx.AASXReader(file) as reader:
+            reader.read_into(object_store=object_store, file_store=file_store)
 
     application = WSGIApp(object_store, file_store, **wsgi_optparams)
 
 else:
-    print(f"STORAGE_TYPE must be either LOCAL_FILE, AASX, JSON or XML! Current value: {storage_type}", file=sys.stderr)
+    print(f"STORAGE_TYPE must be either LOCAL_FILE or AASX! Current value: {storage_type}", file=sys.stderr)
