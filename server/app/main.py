73569bfb4b22b099310d7909f8718582418f57e6
zrgt <garmaev@gmx.de>
2024-08-21 23:39:45 +0200

    app: Add support of JSON and XML files
    - This commit adds support for loading AASX, JSON or XML files.
    So the user can have a mix of these files
    in the storage folder and the application will load them all.
    - Changed types of storage to
    LOCAL_FILE_BACKEND and LOCAL_FILE_READ_ONLY
    - Adapted and refactored README.md and added more examples for running

--- server/app/main.py
+++ server/app/main.py
@@ -2,14 +2,14 @@ import os
 import pathlib
 import sys
 
-from basyx.aas import model
+from basyx.aas import model, adapter
 from basyx.aas.adapter import aasx
 
 from basyx.aas.backend.local_file import LocalFileObjectStore
 from basyx.aas.adapter.http import WSGIApp
 
-storage_path = os.getenv("STORAGE_PATH", "/storage")
-storage_type = os.getenv("STORAGE_TYPE", "AASX")
+storage_path = os.getenv("STORAGE_PATH", "storage")
+storage_type = os.getenv("STORAGE_TYPE", "LOCAL_FILE_READ_ONLY")
 base_path = os.getenv("API_BASE_PATH")
 
 wsgi_optparams = {}
@@ -17,21 +17,30 @@ wsgi_optparams = {}
 if base_path is not None:
     wsgi_optparams["base_path"] = base_path
 
-if storage_type == "LOCAL_FILE":
+if storage_type == "LOCAL_FILE_BACKEND":
     application = WSGIApp(LocalFileObjectStore(storage_path), aasx.DictSupplementaryFileContainer(), **wsgi_optparams)
 
-elif storage_type == "AASX":
+elif storage_type in "LOCAL_FILE_READ_ONLY":
     object_store: model.DictObjectStore = model.DictObjectStore()
     file_store: aasx.DictSupplementaryFileContainer = aasx.DictSupplementaryFileContainer()
 
-    for file in pathlib.Path(storage_path).glob("*.aasx"):
+    for file in pathlib.Path(storage_path).iterdir():
         if not file.is_file():
             continue
         print(f"Loading {file}")
-        with aasx.AASXReader(file) as reader:
-            reader.read_into(object_store=object_store, file_store=file_store)
+
+        if file.suffix == ".json":
+            with open(file) as f:
+                adapter.json.read_aas_json_file_into(object_store, f)
+        elif file.suffix == ".xml":
+            with open(file) as f:
+                adapter.xml.read_aas_xml_file_into(object_store, file)
+        elif file.suffix == ".aasx":
+            with aasx.AASXReader(file) as reader:
+                reader.read_into(object_store=object_store, file_store=file_store)
 
     application = WSGIApp(object_store, file_store, **wsgi_optparams)
 
 else:
-    print(f"STORAGE_TYPE must be either LOCAL_FILE or AASX! Current value: {storage_type}", file=sys.stderr)
+    print(f"STORAGE_TYPE must be either LOCAL_FILE or LOCAL_FILE_READ_ONLY! Current value: {storage_type}",
+          file=sys.stderr)
