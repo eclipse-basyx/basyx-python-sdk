c2e3e69bb41ac256a58292cc9ae8a6ca66e98e16
Wei Guo <w.guo@iat.rwth-aachen.de>
2024-11-08 11:42:00 +0100

    delete test_parse_source

--- sdk/test/backend/test_couchdb.py
+++ sdk/test/backend/test_couchdb.py
@@ -18,32 +18,6 @@ source_core: str = "couchdb://" + TEST_CONFIG["couchdb"]["url"].lstrip("http://"
                    TEST_CONFIG["couchdb"]["database"] + "/"
 
 
-class CouchDBBackendOfflineMethodsTest(unittest.TestCase):
-    def test_parse_source(self):
-        couchdb.register_credentials(url="couchdb.plt.rwth-aachen.de:5984",
-                                     username="test_user",
-                                     password="test_password")
-
-        url = couchdb.CouchDBBackend._parse_source(
-            "couchdbs://couchdb.plt.rwth-aachen.de:5984/path_to_db/path_to_doc"
-        )
-        expected_url = "https://couchdb.plt.rwth-aachen.de:5984/path_to_db/path_to_doc"
-        self.assertEqual(expected_url, url)
-
-        url = couchdb.CouchDBBackend._parse_source(
-            "couchdb://couchdb.plt.rwth-aachen.de:5984/path_to_db/path_to_doc"
-        )
-        expected_url = "http://couchdb.plt.rwth-aachen.de:5984/path_to_db/path_to_doc"
-        self.assertEqual(expected_url, url)
-
-        with self.assertRaises(couchdb.CouchDBSourceError) as cm:
-            couchdb.CouchDBBackend._parse_source("wrong_scheme:plt.rwth-aachen.couchdb:5984/path_to_db/path_to_doc")
-        self.assertEqual("Source has wrong format. "
-                         "Expected to start with {couchdb://, couchdbs://}, got "
-                         "{wrong_scheme:plt.rwth-aachen.couchdb:5984/path_to_db/path_to_doc}",
-                         str(cm.exception))
-
-
 @unittest.skipUnless(COUCHDB_OKAY, "No CouchDB is reachable at {}/{}: {}".format(TEST_CONFIG['couchdb']['url'],
                                                                                  TEST_CONFIG['couchdb']['database'],
                                                                                  COUCHDB_ERROR))
