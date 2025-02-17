6dc956d52ab1403efe85e4704ef4f268448e575d
Sercan Sahin <125310380+Frosty2500@users.noreply.github.com>
2025-02-17 10:31:58 +0100

    scripts.set_copyright_year: Add a CI check to ensure the copyright is updated (#344)
    Previously, there was no check if the copyright
    statement on top of each file (due to the MIT
    license) was up to date. 
    
    This adapts our `update_copyright_year.sh` script
    with a checking mode to determine if any file
    needs adapting and then adds this check to the CI.
    
    At the same time, this also updates all necessary
    files to the year 2025.
    
    Fixes #260
    Fixes #331
    
    ---------
    
    Co-authored-by: s-heppner <iat@s-heppner.com>

--- etc/scripts/set_copyright_year.sh
+++ etc/scripts/set_copyright_year.sh
@@ -9,7 +9,36 @@
 #
 # The script will check the first two lines for a copyright
 # notice (in case the first line is a shebang).
+#
+# Run this script with --check to have it raise an error if it
+# would change anything.
+
+
+# Set CHECK_MODE based on whether --check is passed
+    CHECK_MODE=false
+    if [[ "$1" == "--check" ]]; then
+        CHECK_MODE=true
+        shift # Remove --check from the arguments
+    fi
 
 while read -rd $'\0' year file; do
-    sed -i "1,2s/^\(# Copyright (c) \)[[:digit:]]\{4,\}/\1$year/" "$file"
-done < <(git ls-files -z "$@" | xargs -0I{} git log -1 -z --format="%ad {}" --date="format:%Y" "{}")
+
+    # Extract the first year from the copyright notice
+    current_year=$(sed -n '1,2s/^\(# Copyright (c) \)\([[:digit:]]\{4,\}\).*/\2/p' "$file")
+
+    # Skip the file if no year is found
+    if [[ -z "$current_year" ]]; then
+        continue
+    fi
+
+    if $CHECK_MODE && [[ "$current_year" != "$year" ]]; then
+        echo "Error: Copyright year mismatch in file $file. Expected $year, found $current_year."
+        exit 1
+    fi
+
+    if ! $CHECK_MODE && [[ "$current_year" != "$year" ]]; then
+        sed -i "1,2s/^\(# Copyright (c) \)[[:digit:]]\{4,\}/\1$year/" "$file"
+        echo "Updated copyright year in $file"
+    fi
+done < <(git ls-files -z "$@" | xargs -0I{} git log -1 -z --format="%cd {}" --date="format:%Y" -- "{}")
+
