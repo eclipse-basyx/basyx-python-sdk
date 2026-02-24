fdc5144684bf14b49547519815d52dbbbea1cbf7
Moritz Sommer <m.sommer@iat.rwth-aachen.de>
2026-02-24 08:28:56 +0100

    etc/scripts/set_copyright_year.sh: Fix exit bug and check LICENSE
    Previously, the `EXIT_CODE` of the copyright year check was hardcoded to
    the value 0, causing the CI to pass unconditionally.
    
    This fixes the bug by coupling the `EXIT_CODE` to the result of the
    copyright year check. Moreover, the check has been extended to
    additionally verify the copyright year of the `LICENSE` file.
    
    Fixes #458

--- etc/scripts/set_copyright_year.sh
+++ etc/scripts/set_copyright_year.sh
@@ -2,13 +2,17 @@
 # Usage: ./set_copyright_year.sh [PATHS]
 #
 # This is a small script for setting the correct copyright year
-# for each given file (i.e. the year the file was last changed).
-# Instead of file paths you can also specify directories, in which
-# case the script will attempt to set the copyright year for all
-# files in the given directories. Globbing is also possible.
+# for each given source file (i.e. the year the file was last
+# changed) and LICENSE file (i.e. the latest modification year
+# across all files). Instead of file paths you can also specify
+# directories, in which case the script will attempt to set the
+# copyright year for all files in the given directories.
+# Globbing is also possible.
 #
-# The script will check the first two lines for a copyright
-# notice (in case the first line is a shebang).
+# In source files, the script checks the first two lines for a
+# copyright notice (in case the first line is a shebang).
+# In the LICENSE file, it checks the first three lines for a
+# copyright notice containing a year range.
 #
 # Run this script with --check to have it raise an error if it
 # would change anything.
@@ -17,32 +21,56 @@
 EXIT_CODE=0
 
 # Set CHECK_MODE based on whether --check is passed
-    CHECK_MODE=false
-    if [[ "$1" == "--check" ]]; then
-        CHECK_MODE=true
-        shift # Remove --check from the arguments
-    fi
+CHECK_MODE=false
+if [[ "$1" == "--check" ]]; then
+    CHECK_MODE=true
+    shift # Remove --check from the arguments
+fi
 
+# Initialise a variable to track the latest modification year across all files
+max_year=""
+
+# Validate the copyright year of each source file
 while read -rd $'\0' year file; do
+
+    # Extract the current modification year and update variable
+    if [[ -z "$max_year" || "$year" -gt "$max_year" ]]; then
+        max_year="$year"
+    fi
 
     # Extract the first year from the copyright notice
     current_year=$(sed -n '1,2s/^\(# Copyright (c) \)\([[:digit:]]\{4,\}\).*/\2/p' "$file")
 
-    # Skip the file if no year is found
+    # Skip the source file if no year is found
     if [[ -z "$current_year" ]]; then
         continue
     fi
 
+    # If in check mode, report the incorrect copyright year
     if $CHECK_MODE && [[ "$current_year" != "$year" ]]; then
         echo "Error: Copyright year mismatch in file $file. Expected $year, found $current_year."
-        # Set ERROR_CODE to 1 to indicate mismatch
-	ERROR_CODE=1
+        # Set EXIT_CODE to 1 to indicate mismatch
+        EXIT_CODE=1
     fi
 
+    # Otherwise rewrite the incorrect copyright year
     if ! $CHECK_MODE && [[ "$current_year" != "$year" ]]; then
         sed -i "1,2s/^\(# Copyright (c) \)[[:digit:]]\{4,\}/\1$year/" "$file"
         echo "Updated copyright year in $file"
     fi
 done < <(git ls-files -z "$@" | xargs -0I{} git log -1 -z --format="%cd {}" --date="format:%Y" -- "{}")
+
+# Validate the copyright year of the LICENSE file
+license_current_year=$(sed -n '1,3{s/^Copyright (c) [[:digit:]]\{4\}-\([[:digit:]]\{4\}\).*/\1/p}' LICENSE)
+
+if $CHECK_MODE && [[ -n "$license_current_year" && "$license_current_year" != "$max_year" ]]; then
+    echo "Error: Copyright year mismatch in file LICENSE. Expected $max_year, found $license_current_year."
+    EXIT_CODE=1
+fi
+
+if ! $CHECK_MODE && [[ -n "$license_current_year" && "$license_current_year" != "$max_year" ]]; then
+    sed -i "1,3s/^\(Copyright (c) [[:digit:]]\{4\}-\)[[:digit:]]\{4\}/\1$max_year/" LICENSE
+    echo "Updated copyright year in LICENSE"
+fi
 
 exit $EXIT_CODE
