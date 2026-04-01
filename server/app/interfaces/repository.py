a93ced26635b23c4423ea71889c784bdedee48c8
Ornella33 <ornellamboudia@gmail.com>
2026-04-01 15:18:55 +0200

    Fix MyPy errors

--- server/app/interfaces/repository.py
+++ server/app/interfaces/repository.py
@@ -167,14 +167,14 @@ class WSGIApp(ObjectStoreWSGIApp):
         response: Response = self.handle_request(Request(environ))
         return response(environ, start_response)
 
-    def _get_obj_ts(self, identifier: model.Identifier, type_: Type[model.Identifiable]) \
-            -> model.Identifiable:
+    def _get_obj_ts(self, identifier: model.Identifier, type_: Type[T]) \
+            -> T:
         identifiable = self.object_store.get(identifier)
         if not isinstance(identifiable, type_):
             raise NotFound(f"No {type_.__name__} with {identifier} found!")
         return identifiable
 
-    def _get_all_obj_of_type(self, type_: Type[model.Identifiable]) -> Iterator[model.Identifiable]:
+    def _get_all_obj_of_type(self, type_: Type[T]) -> Iterator[T]:
         for obj in self.object_store:
             if isinstance(obj, type_):
                 yield obj
