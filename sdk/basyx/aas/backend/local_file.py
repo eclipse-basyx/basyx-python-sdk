74e332e2acc9fb537240cc5af216cddc5791c357
zrgt <garmaev@gmx.de>
2026-05-05 15:24:18 +0200

    fix: LocalFileIdentifiableStore.__len__ count only .json files

--- sdk/basyx/aas/backend/local_file.py
+++ sdk/basyx/aas/backend/local_file.py
@@ -150,7 +150,7 @@ class LocalFileIdentifiableStore(model.AbstractObjectStore[model.Identifier, mod
         :return: The number of objects (determined from the number of documents)
         """
         logger.debug("Fetching number of documents from database ...")
-        return len(os.listdir(self.directory_path))
+        return sum(1 for f in os.listdir(self.directory_path) if f.endswith(".json"))
 
     def __iter__(self) -> Iterator[model.Identifiable]:
         """
