aa35c4be53a6ac617dc054466af66506e453b741
s-heppner <iat@s-heppner.com>
2026-05-13 09:16:57 +0200

    Simplify run_discovery storage persistence

--- server/app/services/run_discovery.py
+++ server/app/services/run_discovery.py
@@ -8,35 +8,28 @@ base_path = os.getenv("API_BASE_PATH")
 
 wsgi_optparams = {}
 
+
 if base_path is not None:
     wsgi_optparams["base_path"] = base_path
 
 
-def load_discovery_store(path: str) -> DiscoveryStore:
-    storage_dir = os.path.dirname(path)
-    if storage_dir:
-        os.makedirs(storage_dir, exist_ok=True)
-
-    if os.path.exists(path) and os.path.getsize(path) > 0:
-        return DiscoveryStore.from_file(path)
-
-    discovery_store = DiscoveryStore()
-    discovery_store.to_file(path)
-    return discovery_store
-
-
 # Load DiscoveryStore from disk, if `storage_path` is set
 if storage_path:
-    discovery_store: DiscoveryStore = load_discovery_store(storage_path)
+    # If the storage file exists, we load it
+    if os.path.exists(storage_path) and os.path.getsize(storage_path) > 0:
+        discovery_store = DiscoveryStore.from_file(storage_path)
+
+    # If the file doesn't exist at the given path, we initiate a new file
+    else:
+        os.makedirs(os.path.dirname(storage_path), exist_ok=True)
+        discovery_store = DiscoveryStore()
+        discovery_store.to_file(storage_path)
 else:
     discovery_store = DiscoveryStore()
 
 
 def persist_store():
     if storage_path:
-        storage_dir = os.path.dirname(storage_path)
-        if storage_dir:
-            os.makedirs(storage_dir, exist_ok=True)
         discovery_store.to_file(storage_path)
 
 
