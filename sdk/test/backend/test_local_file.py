eclipse-basyx/basyx-python-sdk

commit 553d87af0cc72139224572163278f33eafa5175c

Refactor `Backend` concept for data persistence  (#370)
Previously, we tried to use the `Backend` concept for both data persistence and data integration. 
This led to complicated cross-dependencies, architectures and badly maintainable code. 

Now, we decided to split these two concepts and use the existing `Backend` `ObjectStore`s for just the data persistence.

Concretely, this commit does the following:

1. Remove the abstract `Backend` class (backends.py), as well as their concrete implementations (`CouchDBBackend`, `LocalFileBackend`), as they are no longer needed
2. Remove the `Referable.source` attribute, as it was only needed for the `Backend` class
3. Remove methods related to the `Referable.source` attribute, such as `Referable.update()` and `Referable.commit()` (base.py), as they were used together to perform the data integration.
4. Adapt other related codes such as tutorials and tests to reflect these changes
5. Adapt documentation to reflect these changes

sdk/basyx/aas/backend/local_file.py
@@ -8,8 +8,8 @@
 This module adds the functionality of storing and retrieving :class:`~basyx.aas.model.base.Identifiable` objects
 in local files.
 
-The :class:`~.LocalFileBackend` takes care of updating and committing objects from and to the files, while the
-:class:`~LocalFileObjectStore` handles adding, deleting and otherwise managing the AAS objects in a specific Directory.
+The :class:`~LocalFileObjectStore` handles adding, deleting and otherwise managing
+the AAS objects in a specific Directory.
 """
 from typing import List, Iterator, Iterable, Union
 import logging
@@ -19,51 +19,11 @@ import hashlib
 import threading
 import weakref
 
-from . import backends
 from ..adapter.json import json_serialization, json_deserialization
 from basyx.aas import model
 
 
 logger = logging.getLogger(__name__)
-
-
-class LocalFileBackend(backends.Backend):
-    """
-    This Backend stores each Identifiable object as a single JSON document as a local file in a directory.
-    Each document's id is build from the object's identifier using a SHA256 sum of its identifiable; the document's
-    contents comprise a single property ``data``, containing the JSON serialization of the BaSyx Python SDK object. The
-    :ref:`adapter.json <adapter.json.__init__>` package is used for serialization and deserialization of objects.
-    """
-
-    @classmethod
-    def update_object(cls,
-                      updated_object: model.Referable,
-                      store_object: model.Referable,
-                      relative_path: List[str]) -> None:
-
-        if not isinstance(store_object, model.Identifiable):
-            raise FileBackendSourceError("The given store_object is not Identifiable, therefore cannot be found "
-                                         "in the FileBackend")
-        file_name: str = store_object.source.replace("file://localhost/", "")
-        with open(file_name, "r") as file:
-            data = json.load(file, cls=json_deserialization.AASFromJsonDecoder)
-            updated_store_object = data["data"]
-            store_object.update_from(updated_store_object)
-
-    @classmethod
-    def commit_object(cls,
-                      committed_object: model.Referable,
-                      store_object: model.Referable,
-                      relative_path: List[str]) -> None:
-        if not isinstance(store_object, model.Identifiable):
-            raise FileBackendSourceError("The given store_object is not Identifiable, therefore cannot be found "
-                                         "in the FileBackend")
-        file_name: str = store_object.source.replace("file://localhost/", "")
-        with open(file_name, "w") as file:
-            json.dump({'data': store_object}, file, cls=json_serialization.AASToJsonEncoder, indent=4)
-
-
-backends.register_backend("file", LocalFileBackend)
 
 
 class LocalFileObjectStore(model.AbstractObjectStore):
@@ -112,7 +72,6 @@ class LocalFileObjectStore(model.AbstractObjectStore):
             with open("{}/{}.json".format(self.directory_path, hash_), "r") as file:
                 data = json.load(file, cls=json_deserialization.AASFromJsonDecoder)
                 obj = data["data"]
-                self.generate_source(obj)
         except FileNotFoundError as e:
             raise KeyError("No Identifiable with hash {} found in local file database".format(hash_)) from e
         # If we still have a local replication of that object (since it is referenced from anywhere else), update that
@@ -120,11 +79,8 @@ class LocalFileObjectStore(model.AbstractObjectStore):
         with self._object_cache_lock:
             if obj.id in self._object_cache:
                 old_obj = self._object_cache[obj.id]
-                # If the source does not match the correct source for this CouchDB backend, the object seems to belong
-                # to another backend now, so we return a fresh copy
-                if old_obj.source == obj.source:
-                    old_obj.update_from(obj)
-                    return old_obj
+                old_obj.update_from(obj)
+                return old_obj
         self._object_cache[obj.id] = obj
         return obj
 
@@ -152,7 +108,6 @@ class LocalFileObjectStore(model.AbstractObjectStore):
             json.dump({"data": x}, file, cls=json_serialization.AASToJsonEncoder, indent=4)
             with self._object_cache_lock:
                 self._object_cache[x.id] = x
-            self.generate_source(x)  # Set the source of the object
 
     def discard(self, x: model.Identifiable) -> None:
         """
@@ -168,7 +123,6 @@ class LocalFileObjectStore(model.AbstractObjectStore):
             raise KeyError("No AAS object with id {} exists in local file database".format(x.id)) from e
         with self._object_cache_lock:
             del self._object_cache[x.id]
-        x.source = ""
 
     def __contains__(self, x: object) -> bool:
         """
@@ -214,23 +168,3 @@ class LocalFileObjectStore(model.AbstractObjectStore):
         Helper method to represent an ASS Identifier as a string to be used as Local file document id
         """
         return hashlib.sha256(identifier.encode("utf-8")).hexdigest()
-
-    def generate_source(self, identifiable: model.Identifiable) -> str:
-        """
-        Generates the source string for an :class:`~basyx.aas.model.base.Identifiable` object that is backed by the File
-
-        :param identifiable: Identifiable object
-        """
-        source: str = "file://localhost/{}/{}.json".format(
-            self.directory_path,
-            self._transform_id(identifiable.id)
-        )
-        identifiable.source = source
-        return source
-
-
-class FileBackendSourceError(Exception):
-    """
-    Raised, if the given object's source is not resolvable as a local file
-    """
-    pass

sdk/basyx/aas/model/provider.py
@@ -65,8 +65,9 @@ class AbstractObjectStore(AbstractObjectProvider, MutableSet[_IT], Generic[_IT],
 
     ObjectStores are special ObjectProvides that – in addition to retrieving objects by
     :class:`~basyx.aas.model.base.Identifier` – allow to add and delete objects (i.e. behave like a Python set).
-    This includes local object stores (like :class:`~.DictObjectStore`) and database
-    :class:`Backends <basyx.aas.backend.backends.Backend>`.
+    This includes local object stores (like :class:`~.DictObjectStore`) and specific object stores
+    (like :class:`~basyx.aas.backend.couchdb.CouchDBObjectStore` and
+    :class `~basyx.aas.backend.local_file.LocalFileObjectStore`).
 
     The AbstractObjectStore inherits from the :class:`~collections.abc.MutableSet` abstract collections class and
     therefore implements all the functions of this class.

sdk/test/backend/test_local_file.py
@@ -31,10 +31,8 @@ class LocalFileBackendTest(unittest.TestCase):
     def test_object_store_add(self):
         test_object = create_example_submodel()
         self.object_store.add(test_object)
-        self.assertEqual(
-            test_object.source,
-            source_core+"fd787262b2743360f7ad03a3b4e9187e4c088aa37303448c9c43fe4c973dac53.json"
-        )
+        # Note that this test is only checking that there are no errors during adding.
+        # The actual logic is tested together with retrieval in `test_retrieval`.
 
     def test_retrieval(self):
         test_object = create_example_submodel()
@@ -48,11 +46,6 @@ class LocalFileBackendTest(unittest.TestCase):
         del test_object
         test_object_retrieved_again = self.object_store.get_identifiable('https://acplt.org/Test_Submodel')
         self.assertIs(test_object_retrieved, test_object_retrieved_again)
-
-        # However, a changed source should invalidate the cached object, so we should get a new copy
-        test_object_retrieved.source = "couchdb://example.com/example/IRI-https%3A%2F%2Facplt.org%2FTest_Submodel"
-        test_object_retrieved_third = self.object_store.get_identifiable('https://acplt.org/Test_Submodel')
-        self.assertIsNot(test_object_retrieved, test_object_retrieved_third)
 
     def test_example_submodel_storing(self) -> None:
         example_submodel = create_example_submodel()
@@ -111,16 +104,3 @@ class LocalFileBackendTest(unittest.TestCase):
             self.object_store.discard(retrieved_submodel)
         self.assertEqual("'No AAS object with id https://acplt.org/Test_Submodel exists in "
                          "local file database'", str(cm.exception))
-
-    def test_editing(self):
-        test_object = create_example_submodel()
-        self.object_store.add(test_object)
-
-        # Test if commit uploads changes
-        test_object.id_short = "SomeNewIdShort"
-        test_object.commit()
-
-        # Test if update restores changes
-        test_object.id_short = "AnotherIdShort"
-        test_object.update()
-        self.assertEqual("SomeNewIdShort", test_object.id_short)

sdk/basyx/aas/backend/__init__.py
@@ -1,7 +1,3 @@
 """
-This module implements a standardized way of integrating data from existing systems into AAS objects. To achieve this,
-the abstract :class:`~basyx.aas.backend.backends.Backend` class implements the class methods
-:meth:`~basyx.aas.backend.backends.Backend.update_object` and :meth:`~basyx.aas.backend.backends.Backend.commit_object`,
-which every implementation of a backend needs to overwrite. For a tutorial on how to implement a backend, see
-:ref:`this tutorial <tutorial_backend_couchdb>`
+This module implements a standardized way of persisting AAS objects using various backends.
 """

sdk/basyx/aas/backend/backends.py
@@ -1,175 +1,0 @@
-# Copyright (c) 2025 the Eclipse BaSyx Authors
-#
-# This program and the accompanying materials are made available under the terms of the MIT License, available in
-# the LICENSE file of this project.
-#
-# SPDX-License-Identifier: MIT
-"""
-This module provides a registry and abstract base class for Backends. A :class:`~.Backend` is a class that allows to
-synchronize Referable AAS objects or their included data with external data sources such as a remote API or a local
-source for real time data. Each backend provides access to one kind of data source.
-
-The data source of an individual object is specified as a URI in its ``source`` attribute. The schema part of that URI
-defines the type of data source and, in consequence, the backend class to use for synchronizing this object.
-
-Custom backends for additional types of data sources can be implemented by subclassing :class:`Backend` and
-implementing the :meth:`~.Backend.commit_object` and :meth:`~.Backend.update_object` class methods. These are used
-internally by the objects' :meth:`~basyx.aas.model.base.Referable.update` and
-:meth:`~basyx.aas.model.base.Referable.commit` methods when the backend is applicable for the relevant source URI.
-Then, the Backend class needs to be registered to handle update/commit requests for a specific URI schema, using
-:meth:`~basyx.aas.backend.backends.register_backend`.
-"""
-import abc
-import re
-from typing import List, Dict, Type, TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from ..model import Referable
-
-
-class Backend(metaclass=abc.ABCMeta):
-    """
-    Abstract base class for all Backend classes.
-
-    Each Backend class is typically capable of synchronizing (updating/committing) objects with a type of external data
-    source, identified by one or more source URI schemas. Custom backends for custom source URI schemas should inherit
-    from this class and be registered via :meth:`~basyx.aas.backend.backends.register_backend`. to be used by Referable
-    object's :meth:`~basyx.aas.model.base.Referable.update` and :meth:`~basyx.aas.model.base.Referable.commit` methods
-    when required.
-    """
-
-    @classmethod
-    @abc.abstractmethod
-    def commit_object(cls,
-                      committed_object: "Referable",
-                      store_object: "Referable",
-                      relative_path: List[str]) -> None:
-        """
-        Function (class method) to be called when an object shall be committed (local changes pushed to the external
-        data source) via this backend implementation.
-
-        It is automatically called by the :meth:`~basyx.aas.model.base.Referable.commit` implementation, when the source
-        URI of the object or the source URI one of its ancestors in the AAS object containment hierarchy include a
-        URI schema for which this
-        backend has been registered. Both of the objects are passed to this function: the one which shall be committed
-        (``committed_object``) and its ancestor with the relevant source URI (``store_object``). They may be the same,
-        the committed object has a source with the relevant schema itself. Additionally, the ``relative_path`` from the
-        ``store_object`` down to the ``committed_object`` is provided.
-
-        The backend MUST ensure to commit all local changes of at least the ``committed_object`` and all objects
-        contained within it (if any) to the data source. It MAY additionally commit changes to other objects (i.e. the
-        ``store_object`` and any additional contained object).
-
-        For this purpose a concrete implementation of this method would typically use the ``source`` attribute of the
-        ``store_object`` to identify the data source. If the data source supports fine-grained access to contained
-        objects, the ``relative_path`` may become handy to compose the committed object's address within the data
-        source's interface.
-
-        :param committed_object: The object which shall be synced to the external data source
-        :param store_object: The object which originates from the relevant data source (i.e. has the relevant source
-            attribute). It may be the ``committed_object`` or one of its ancestors in the AAS object hierarchy.
-        :param relative_path: List of idShort strings to resolve the ``committed_object`` starting at the
-            ``store_object``, such that `obj = store_object; for i in relative_path: obj = obj.get_referable(i)`
-            resolves to the ``committed_object``. In case that ``store_object is committed_object``, it is an empty
-            list.
-        :raises BackendNotAvailableException: when the external data source cannot be reached
-        """
-        pass
-
-    @classmethod
-    @abc.abstractmethod
-    def update_object(cls,
-                      updated_object: "Referable",
-                      store_object: "Referable",
-                      relative_path: List[str]) -> None:
-        """
-        Function (class method) to be called when an object shall be updated (local object updated with changes from the
-        external data source) via this backend implementation.
-
-        It is automatically called by the :meth:`~basyx.aas.model.base.Referable.update` implementation,
-        when the source URI of the object or the source URI one of its ancestors in the AAS object containment hierarchy
-        include a URI schema for which this backend has been registered. Both of the objects are passed
-        to this function: the one which shall be update (``updated_object``) and its ancestor with
-        the relevant source URI (``store_object``). They may be the same, the updated object has a source with
-        the relevant schema itself. Additionally, the ``relative_path`` from the ``store_object`` down to
-        the ``updated_object`` is provided.
-
-        The backend MUST ensure to update at least the ``updated_object`` and all objects contained within it (if any)
-        with any changes from the data source. It MAY additionally update other objects (i.e. the ``store_object`` and
-        any additional contained object).
-
-        For this purpose a concrete implementation of this method would typically use the ``source`` attribute of the
-        ``store_object`` to identify the data source. If the data source supports fine-grained access to contained
-        objects, the ``relative_path`` may become handy to compose the updated object's address within the data source's
-        interface.
-
-        :param updated_object: The object which shall be synced from the external data source
-        :param store_object: The object which originates from the relevant data source (i.e. has the relevant source
-            attribute). It may be the ``committed_object`` or one of its ancestors in the AAS object hierarchy.
-        :param relative_path: List of idShort strings to resolve the ``updated_object`` starting at the
-            ``store_object``, such that `obj = store_object; for i in relative_path: obj = obj.get_referable(i)`
-            resolves to the ``updated_object``. In case that ``store_object is updated_object``, it is an empty list.
-        :raises BackendNotAvailableException: when the external data source cannot be reached
-        """
-        pass
-
-
-# Global registry for backends by URI scheme
-# TODO allow multiple backends per scheme with priority
-_backends_map: Dict[str, Type[Backend]] = {}
-
-
-def register_backend(scheme: str, backend_class: Type[Backend]) -> None:
-    """
-    Register a Backend implementation to handle update/commit operations for a specific type of external data sources,
-    identified by a source URI schema.
-
-    This method may be called multiple times for a single Backend class, to register that class as a backend
-    implementation for different source URI schemas (e.g. use the same backend for 'http://' and 'https://' sources).
-
-    :param scheme: The URI schema of source URIs to be handled with Backend class, without trailing colon and slashes.
-        E.g. 'http', 'https', 'couchdb', etc.
-    :param backend_class: The Backend implementation class. Should inherit from :class:`Backend`.
-    """
-    # TODO handle multiple backends per scheme
-    _backends_map[scheme] = backend_class
-
-
-RE_URI_SCHEME = re.compile(r"^([a-zA-Z][a-zA-Z+\-\.]*):")
-
-
-def get_backend(url: str) -> Type[Backend]:
-    """
-    Internal function to retrieve the Backend implementation for the external data source identified by the given
-    ``url`` via the url's schema.
-
-    :param url: External data source URI to find an appropriate Backend implementation for
-    :return: A Backend class, capable of updating/committing from/to the external data source
-    :raises UnknownBackendException: When no backend is available for that url
-    """
-    # TODO handle multiple backends per scheme
-    scheme_match = RE_URI_SCHEME.match(url)
-    if not scheme_match:
-        raise ValueError("{} is not a valid URL with URI scheme.".format(url))
-    scheme = scheme_match[1]
-    try:
-        return _backends_map[scheme]
-    except KeyError as e:
-        raise UnknownBackendException("Could not find Backend for source '{}'".format(url)) from e
-
-
-# #################################################################################################
-# Custom Exception classes for reporting errors during interaction with Backends
-class BackendError(Exception):
-    """Base class of all exceptions raised by the backends module"""
-    pass
-
-
-class UnknownBackendException(BackendError):
-    """Raised, if the backend is not found in the registry"""
-    pass
-
-
-class BackendNotAvailableException(BackendError):
-    """Raised, if the backend does exist in the registry, but is not available for some reason"""
-    pass

commit 6084f69bf4909e56f712ff11b52d035948948372

sdk: Fix `LocalFileIdentifiableStore` mutation persistence
Previously, mutations to objects retrieved from
`LocalFileIdentifiableStore` were never written back to disk.
Only `add()` wrote to the file store; subsequent in-memory changes
were invisible to other processes (e.g. different uWSGI workers)
once the `WeakValueDictionary` cache was evicted.

This was an issue introduced in #370, which removed
`Referable.commit()` and the `LocalFileBackend` class without
replacing the write-back mechanism.

Two fixes are made here:

- `AbstractObjectStore.commit()` is added as a no-op default.
  `LocalFileIdentifiableStore` overrides it to re-serialize the
  object to its `.json` file.
- `get_item()` now returns the cached object directly on a cache
  hit, instead of always re-reading from disk and overwriting
  in-memory state. This was a secondary bug that would silently
  discard mutations within the same process if the same object
  was retrieved more than once.

A regression test (`test_mutation_persistence`) is added that
mutates a stored object, evicts the cache via `gc.collect()`,
and verifies the updated value is read back from disk.

sdk/basyx/aas/backend/local_file.py
@@ -68,21 +68,14 @@ class LocalFileIdentifiableStore(model.AbstractObjectStore[model.Identifier, mod
 
         :raises KeyError: If the respective file could not be found
         """
-        # Try to get the correct file
         try:
             with open("{}/{}.json".format(self.directory_path, hash_), "r") as file:
                 data = json.load(file, cls=json_deserialization.AASFromJsonDecoder)
                 obj = data["data"]
         except FileNotFoundError as e:
             raise KeyError("No Identifiable with hash {} found in local file database".format(hash_)) from e
-        # If we still have a local replication of that object (since it is referenced from anywhere else), update that
-        # replication and return it.
         with self._object_cache_lock:
-            if obj.id in self._object_cache:
-                old_obj = self._object_cache[obj.id]
-                old_obj.update_from(obj)
-                return old_obj
-        self._object_cache[obj.id] = obj
+            self._object_cache[obj.id] = obj
         return obj
 
     def get_item(self, identifier: model.Identifier) -> model.Identifiable:
@@ -91,6 +84,9 @@ class LocalFileIdentifiableStore(model.AbstractObjectStore[model.Identifier, mod
 
         :raises KeyError: If the respective file could not be found
         """
+        with self._object_cache_lock:
+            if identifier in self._object_cache:
+                return self._object_cache[identifier]
         try:
             return self.get_identifiable_by_hash(self._transform_id(identifier))
         except KeyError as e:
@@ -109,6 +105,18 @@ class LocalFileIdentifiableStore(model.AbstractObjectStore[model.Identifier, mod
             json.dump({"data": x}, file, cls=json_serialization.AASToJsonEncoder, indent=4)
             with self._object_cache_lock:
                 self._object_cache[x.id] = x
+
+    def commit(self, x: model.Identifiable) -> None:
+        """
+        Write the current in-memory state of a stored object back to its file.
+
+        :param x: The object to persist
+        :raises KeyError: If the object is not present in the store
+        """
+        if not os.path.exists("{}/{}.json".format(self.directory_path, self._transform_id(x.id))):
+            raise KeyError("No AAS object with id {} exists in local file database".format(x.id))
+        with open("{}/{}.json".format(self.directory_path, self._transform_id(x.id)), "w") as file:
+            json.dump({"data": x}, file, cls=json_serialization.AASToJsonEncoder, indent=4)
 
     def discard(self, x: model.Identifiable) -> None:
         """

sdk/basyx/aas/model/provider.py
@@ -59,6 +59,18 @@ class AbstractObjectStore(AbstractObjectProvider[_KEY, _VALUE], MutableSet[_VALU
     def __init__(self):
         pass
 
+    def commit(self, x: _VALUE) -> None:
+        """
+        Persist an in-memory mutation of a stored object back to the underlying storage.
+
+        The default implementation is a no-op, suitable for in-memory stores where the object
+        is the storage. Persistent backends (e.g. file-based or database-backed stores) must
+        override this to write the updated object back to storage.
+
+        :param x: The object whose current in-memory state should be persisted
+        """
+        pass
+
     def update(self, other: Iterable[_VALUE]) -> None:
         for x in other:
             self.add(x)

sdk/test/backend/test_local_file.py
@@ -4,6 +4,7 @@
 # the LICENSE file of this project.
 #
 # SPDX-License-Identifier: MIT
+import gc
 import os.path
 import shutil
 
@@ -133,6 +134,32 @@ class LocalFileBackendTest(TestCase):
         items = list(self.identifiable_store)
         self.assertEqual(5, len(items))
         os.remove(stray)
+
+    def test_mutation_persistence(self) -> None:
+        submodel = model.Submodel(
+            id_='https://example.org/MutationTest',
+            submodel_element={
+                model.Property(id_short='Prop', value_type=model.datatypes.String, value='before')
+            }
+        )
+        self.identifiable_store.add(submodel)
+
+        retrieved = self.identifiable_store.get_item('https://example.org/MutationTest')
+        assert isinstance(retrieved, model.Submodel)
+        prop = retrieved.get_referable(['Prop'])
+        assert isinstance(prop, model.Property)
+        prop.update_from(model.Property(id_short='Prop', value_type=model.datatypes.String, value='after'))
+        self.identifiable_store.commit(retrieved)
+
+        # Drop all strong references to evict the WeakValueDictionary cache
+        del submodel, retrieved, prop
+        gc.collect()
+
+        fresh = self.identifiable_store.get_item('https://example.org/MutationTest')
+        assert isinstance(fresh, model.Submodel)
+        fresh_prop = fresh.get_referable(['Prop'])
+        assert isinstance(fresh_prop, model.Property)
+        self.assertEqual('after', fresh_prop.value)
 
     def test_reload_discard(self) -> None:
         # Load example submodel