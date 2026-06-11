f7ac085b4cbb74b143796c762206651d34254f12
Paul Gerber <p.gerber@iat.rwth-aachen.de>
2026-06-11 11:55:36 +0200

    Translate comments to English

--- sdk/test/backend/test_local_file.py
+++ sdk/test/backend/test_local_file.py
@@ -67,21 +67,22 @@ class LocalFileBackendTest(TestCase):
         self.assertNotIn(example_submodel, self.identifiable_store)
 
     def test_check_directory(self) -> None:
-        # Erzwinge einen nicht-existierenden Ordner (lösche setUp-Ergebnis)
+        # Make sure the test directory does not exist at the beginning of the test
         if os.path.exists(store_path):
             shutil.rmtree(store_path)
 
-        # Ohne create=True muss ein FileNotFoundError geworfen werden
+        # If create=False, check_directory should raise a FileNotFoundError,
+        # if the directory does not exist
         with self.assertRaises(FileNotFoundError) as cm:
             self.identifiable_store.check_directory(create=False)
         expected_error = "The given directory ({}) does not exist".format(store_path)
         self.assertEqual(expected_error, str(cm.exception))
 
-        # Mit create=True muss der Ordner erfolgreich erstellt werden
+        # If create=True, check_directory should create the directory if it does not exist
         self.identifiable_store.check_directory(create=True)
         self.assertTrue(os.path.exists(store_path))
 
-        # Wenn der Ordner existiert, darf create=False keinen Fehler mehr werfen
+        # If the directory exists, create=False should not raise an error
         self.identifiable_store.check_directory(create=False)
 
     def test_iterating(self) -> None:
