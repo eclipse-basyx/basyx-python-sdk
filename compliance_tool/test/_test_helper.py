9d617a4fabd2bc6684d3c4b27412c4f6d405fdda
hpoeche <h.poeche@iat.rwth-aachen.de>
2026-06-15 20:01:36 +0200

    compliance_tool: refactor and extend aasx tests
    Tests for the compliance check for aasx files were refactored so
    they use mocking instead of actual files. Additional test cases
    were added to ensure
    
     1. alignment of core properties is checked
     2. content of supplementary files is equal

--- compliance_tool/test/_test_helper.py
+++ compliance_tool/test/_test_helper.py
@@ -1,5 +1,37 @@
+import io
 from typing import Literal, Type, Optional
+import datetime
 import logging
+
+import pyecma376_2
+
+from basyx.aas.examples.data import create_example_aas_binding, TEST_PDF_FILE
+
+
+def create_example_aas_core_properties() -> pyecma376_2.OPCCoreProperties:
+    cp = pyecma376_2.OPCCoreProperties()
+    cp.created = datetime.datetime(2020, 1, 1, 0, 0, 0)
+    cp.creator = "Eclipse BaSyx Python Testing Framework"
+    cp.description = "Test_Description"
+    cp.lastModifiedBy = "Eclipse BaSyx Python Testing Framework Compliance Tool"
+    cp.modified = datetime.datetime(2020, 1, 1, 0, 0, 1)
+    cp.revision = "1.0"
+    cp.version = "2.0.1"
+    cp.title = "Test Title"
+    return cp
+
+
+def create_read_into_mock(file: Literal['TestFile', 'TestFileWrong', None]):
+    def fill_stores (store, file_store, **kwargs) -> None:
+        for item in create_example_aas_binding():
+            store.add(item)
+
+        if file == 'TestFile':
+            with open(TEST_PDF_FILE, 'rb') as f:
+                file_store.add_file("/TestFile.pdf", f, "application/pdf")
+        elif file == 'TestFileWrong':
+            file_store.add_file("/TestFile.pdf", io.BytesIO(b"dummy"), "application/pdf")
+    return fill_stores
 
 def create_mock_effect(
         module: str,
