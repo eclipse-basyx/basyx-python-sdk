79625a93f08e1ca0ba214ad040b66b047e28b485
hpoeche <h.poeche@iat.rwth-aachen.de>
2026-06-16 21:11:27 +0200

    compliance_tool: fix aasx file equivalence
    Previously the aasx file equivalence check would
     1. still execute subsequent steps if the loading of one file fails.
        This was caused by checking only if `state_manager.status is Status.FAILED`,
        missing that `Status.NOT_EXECUTED > Status.FAILED`.
     2. use blank assertions to ensure core_properties `created` attribute
        is of type `datetime.datetime`.
        This resulted in the compliance_tool failing with `AssertionError`
        in cases that were caused by (1).
    
    Status guards for fast-failing are now corrected to take
    `Status.NOT_EXECUTED` into account.
    The assertions are removed and replaced with `DataChecker` checks
    to inform the user gracefully on problems. `cast(...)` is used
    to inform the type-checker about the `isinstance(...)` result.

--- compliance_tool/aas_compliance_tool/compliance_check_aasx.py
+++ compliance_tool/aas_compliance_tool/compliance_check_aasx.py
@@ -13,7 +13,7 @@ All functions reports any issues using the given
 """
 import datetime
 import logging
-from typing import Optional, Tuple
+from typing import Optional, Tuple, cast
 import io
 from lxml import etree  # type: ignore
 
@@ -224,7 +224,7 @@ def check_aasx_files_equivalence(file_path_1: str, file_path_2: str, state_manag
 
     identifiable_store_2, files_2, cp_2 = check_deserialization(file_path_2, state_manager, 'second')
 
-    if state_manager.status is Status.FAILED:
+    if state_manager.status >= Status.FAILED:
         state_manager.add_step('Check if data in files are equal')
         state_manager.set_step_status(Status.NOT_EXECUTED)
         state_manager.add_step('Check if core properties are equal')
@@ -244,18 +244,30 @@ def check_aasx_files_equivalence(file_path_1: str, file_path_2: str, state_manag
 
     state_manager.add_log_records_from_data_checker(checker)
 
-    if state_manager.status is Status.FAILED:
+    if state_manager.status >= Status.FAILED:
         state_manager.add_step('Check if core properties are equal')
         state_manager.set_step_status(Status.NOT_EXECUTED)
         return
 
     state_manager.add_step('Check if core properties are equal')
     checker2 = DataChecker(raise_immediately=False)
-    assert (isinstance(cp_1.created, datetime.datetime))
-    assert (isinstance(cp_2.created, datetime.datetime))
-    duration = cp_1.created - cp_2.created
+    checker2.check(isinstance(cp_1.created, datetime.datetime),
+                   "core property created of first file must be of type datetime",
+                   created=type(cp_1.created))
+    checker2.check(isinstance(cp_2.created, datetime.datetime),
+                   "core property created of second file must be of type datetime",
+                   created=type(cp_2.created))
+
+    if any(True for _ in checker2.failed_checks):
+        state_manager.add_log_records_from_data_checker(checker2)
+        return
+
+    duration = cast(datetime.datetime, cp_1.created) - cast(datetime.datetime, cp_2.created)
     checker2.check(duration.microseconds < 20, "created must be {}".format(cp_1.created), value=cp_2.created)
     checker2.check(cp_1.creator == cp_2.creator, "creator must be {}".format(cp_1.creator), value=cp_2.creator)
     checker2.check(cp_1.lastModifiedBy == cp_2.lastModifiedBy, "lastModifiedBy must be {}".format(cp_1.lastModifiedBy),
                    value=cp_2.lastModifiedBy)
+    checker2.check(cp_1.revision == cp_2.revision, "revision must be {}".format(cp_2.revision), revision=cp_1.revision)
+    checker2.check(cp_1.version == cp_2.version, "version must be {}".format(cp_2.version), version=cp_1.version)
+    checker2.check(cp_1.title == cp_2.title, "title must be {}".format(cp_2.title), title=cp_1.title)
     state_manager.add_log_records_from_data_checker(checker2)
