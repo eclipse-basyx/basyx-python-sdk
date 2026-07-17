4997f8ab9d387fc0e4a9c64a4c2114e828ed9fbf
Joshua Benning <j.benning@iat.rwth-aachen.de>
2026-07-17 15:26:31 +0200

    Fix empy line after docstring (D204)

--- compliance_tool/aas_compliance_tool/state_manager.py
+++ compliance_tool/aas_compliance_tool/state_manager.py
@@ -25,6 +25,7 @@ class Status(enum.IntEnum):
     :cvar FAILED:
     :cvar NOT_EXECUTED:
     """
+
     SUCCESS = 0
     SUCCESS_WITH_WARNINGS = 1  # never used
     FAILED = 2
@@ -39,6 +40,7 @@ class Step:
     :ivar status: Status of the step from type Status
     :ivar log_list: List of :class:`LogRecords <logging.LogRecord>` which belong to this step
     """
+
     def __init__(self, name: str, status: Status, log_list: List[logging.LogRecord]):
         self.name = name
         self.status = status
@@ -65,6 +67,7 @@ class ComplianceToolStateManager(logging.Handler):
 
     :ivar steps: List of :class:`Steps <.Step>`
     """
+
     def __init__(self):
         """
         steps: List of steps. Each step consist of a step name, a step status and LogRecords belong to to this step.
